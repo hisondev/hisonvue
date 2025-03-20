78f89db9741b5306c5559e9c8acd348d3b64c6b8
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-20 16:52:25 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import HisonVue from '../../dist/hisonvue.es.js'
+import { HisonVue } from 'hisonvue'
+import 'hisonvue/style.css'
 
 const app = createApp(App)
 app.use(HisonVue)
