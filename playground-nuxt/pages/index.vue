da85597b753c5091b855b3fb76f882297cc51b82
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-20 16:54:39 +0900

    Create index.vue

---
+++ playground-nuxt/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Nuxt Playground Test</h1>
+        <HButton @click="onClick">Click Me (Nuxt)</HButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+const onClick = () => {
+    alert('HButton Clicked in Nuxt Playground!')
+}
+</script>
