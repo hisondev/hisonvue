145ff093968001bc3601769db7b2bc63f023ae29
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-20 16:49:11 +0900

    Update HButton.vue

--- src/components/HButton.vue
+++ src/components/HButton.vue
@@ -4,14 +4,16 @@
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+
+</script>
 
 <style scoped>
 .hison-btn {
   padding: 0.5rem 1rem;
   border-radius: 6px;
-  background-color: var(--primary-color, #42b983);
-  color: white;
+  background-color: var(--primary-color, #000);
+  color: #fff;
   border: none;
   cursor: pointer;
 }
