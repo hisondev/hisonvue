606bc0ef1dc901a0be5c190e9c5d2591c50eda02
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-21 16:46:29 +0900

    Update module.mjs

--- nuxt/module.mjs
+++ nuxt/module.mjs
@@ -1,4 +1,7 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addTypeTemplate } from '@nuxt/kit'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
 export default defineNuxtModule({
   meta: {
@@ -8,5 +11,18 @@ export default defineNuxtModule({
   setup(_, nuxt) {
     const resolver = createResolver(import.meta.url)
     addPlugin(resolver.resolve('./plugin.mjs'))
+
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = dirname(__filename)
+    const globalComponentsPath = resolve(__dirname, '../dist/hisonvue.d.ts')  // ✅ 여기로 변경!
+
+    addTypeTemplate({
+      filename: 'types/hisonvue-components.d.ts',
+      getContents: () => readFileSync(globalComponentsPath, 'utf-8')
+    })
+
+    nuxt.hook('prepare:types', (opts) => {
+      opts.references.push({ path: resolver.resolve('types/hisonvue-components.d.ts') })
+    })
   }
 })
