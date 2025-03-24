7b9fb62573d1bead1c019be044fe02783f33e03f
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-24 16:59:51 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import { App } from 'vue'
 import { createSSRClientOnly } from './utils/createSSRClientOnly'
+import type { HisonVueConfig } from './types'
 
 const HButton = createSSRClientOnly<typeof import('./components/HButton.vue').default>(
   () => import('./components/HButton.vue'),
   'HButton'
 )
 
+const HEditor = createSSRClientOnly<typeof import('./components/HEditor.vue').default>(
+  () => import('./components/HEditor.vue'),
+  'HEditor'
+)
+
+const HProvider = createSSRClientOnly<typeof import('./components/HProvider.vue').default>(
+  () => import('./components/HProvider.vue'),
+  'HProvider'
+)
+
 export const HisonVue = {
-  install(app: App) {
-    app.component('HButton', HButton)
+  install(app: App, config?: HisonVueConfig) {
+    app.provide('hisonvue-config', config || {});
+    app.component('HButton', HButton);
+    app.component('HEditor', HEditor);
+    app.component('HProvider', HProvider);
   }
 }
 
 export {
-  HButton
+  HButton,
+  HEditor,
+  HProvider,
 }
+
+export type { HisonVueConfig } from './types'
