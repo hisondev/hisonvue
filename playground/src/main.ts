37cd9adc9d4f79b03414a28649823c33ab562698
hisondev <64723261+hisondev@users.noreply.github.com>
2025-03-24 16:56:39 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { HisonVue } from 'hisonvue'
+import { HisonVue, type HisonVueConfig } from 'hisonvue'
 import 'hisonvue/style.css'
 
 const app = createApp(App)
-app.use(HisonVue)
+
+const hisonVueConfig: HisonVueConfig = {
+    primaryColor: '#ccddcc',
+    size: 's',
+}
+app.use(HisonVue, hisonVueConfig)
 app.mount('#app')
