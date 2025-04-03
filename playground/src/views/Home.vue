591a5e8b5bc129dc8e1235e920e96467f63e345c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-03 16:55:42 +0900

    Update Home.vue

--- playground/src/views/Home.vue
+++ playground/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
-    <HProvider>
-        <h1>Home Page</h1>
-        <HButton @click="goToTest" text-color="#007bff">Go to Test Page</HButton>
-        <HGrid></HGrid>
-    </HProvider>
+    <h1>Home Page</h1>
+    <HButton @click="goToTest" text-color="#007bff">Go to Test Page</HButton>
+    <HGrid></HGrid>
 </template>
 
 <script setup lang="ts">
