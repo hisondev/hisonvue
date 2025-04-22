098c1f200d01dc0002f88b44550faa2de58f6726
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-22 16:52:21 +0900

    Update plugin.mjs

--- nuxt/plugin.mjs
+++ nuxt/plugin.mjs
@@ -1,9 +1,9 @@
 import { defineNuxtPlugin } from '#app'
-import { HisonVue } from 'hisonvue'
-import 'hisonvue/style.css'   // CSS 자동 불러오기
+import { hisonVue } from 'hisonvue'
+import 'hisonvue/style.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.client) {
-    nuxtApp.vueApp.use(HisonVue)
+    nuxtApp.vueApp.use(hisonVue)
   }
 })
