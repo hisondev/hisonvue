caeecfbc2a3e3d053518afa937a4dc02f89fbf9d
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-23 16:51:38 +0900

    Update index.vue

--- playground-nuxt/pages/index.vue
+++ playground-nuxt/pages/index.vue
@@ -1,12 +1,15 @@
 <template>
     <h1>Nuxt Home Page</h1>
-    <HButton @click="goToTest" text-color="#007bff">Go to Test Page</HButton>
-    <HGrid></HGrid>
+    <HButton @click="goToGridTest" text-color="#007bff">Go to Grid Test Page</HButton><br><br>
+    <HButton @click="goToNoteTest" text-color="#007bff">Go to Note Test Page</HButton>
 </template>
 
 <script setup lang="ts">
 const router = useRouter()
-    const goToTest = () => {
-    router.push('/test001')
+const goToGridTest = () => {
+    router.push('/gridTest')
+}
+const goToNoteTest = () => {
+    router.push('/noteTest')
 }
 </script>
