9584b7d821acaa5695a62a2b6347c27b9747e704
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-23 16:52:12 +0900

    Update hisonvue.client.ts

--- playground-nuxt/plugins/hisonvue.client.ts
+++ playground-nuxt/plugins/hisonvue.client.ts
@@ -1,12 +1,12 @@
 import { defineNuxtPlugin } from '#app'
-import { hisonVue, type HisonVueConfig } from 'hisonvue'
+import { hisonvueApp, type HisonvueConfig } from 'hisonvue'
 import 'hisonvue/style.css'
 
-const hisonVueConfig: HisonVueConfig = {
+const hisonvueConfig: HisonvueConfig = {
   primaryColor: '#ccddcc',
   size: 's',
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(hisonVue, hisonVueConfig)
+  nuxtApp.vueApp.use(hisonvueApp, hisonvueConfig)
 })
