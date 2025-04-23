bb28c033591fd736f0cf2791f7d130951a1356fd
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-23 16:53:07 +0900

    Update Home.vue

--- playground/src/views/Home.vue
+++ playground/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
     <h1>Home Page</h1>
-    <HButton @click="goToTest" text-color="#007bff">Go to Test Page</HButton>
-    <HGrid></HGrid>
+    <HButton @click="goToGridTest">Go to Grid Test Page</HButton><br><br>
+    <HButton @click="goToNoteTest">Go to Note Test Page</HButton>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 const router = useRouter()
-const goToTest = () => {
-    router.push('/test')
+const goToGridTest = () => {
+    router.push('/gridTest')
+}
+const goToNoteTest = () => {
+    router.push('/noteTest')
 }
 </script>
