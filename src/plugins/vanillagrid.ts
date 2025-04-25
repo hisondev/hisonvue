3dc490ed65bea8eb1ec96bcb50ada08dd8d1053c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-25 16:59:15 +0900

    Create vanillagrid.ts

---
+++ src/plugins/vanillagrid.ts
@@ -0,0 +1,37 @@
+import { getVanillagrid, getVanillagridConfig, type VanillagridConfig } from 'vanillagrid2'
+import type { HisonConfig } from '../types'
+import { isValidHexColor } from '../utils';
+import { Size } from '../enums';
+
+export const getVg = (hisonConfig: HisonConfig) => {
+    const vgConfig = getVanillagridConfig();
+    setVnConfigWithHisonConfig(vgConfig, hisonConfig);
+    const vg = getVanillagrid(vgConfig);
+
+    return vg;
+}
+
+const setVnConfigWithHisonConfig = (vgConfig: VanillagridConfig, hisonConfig: HisonConfig) => {
+    if(hisonConfig.primaryColor) {
+        if (!isValidHexColor(hisonConfig.primaryColor)) {
+          throw new Error(`[Hisonvue] Invalid mainColor: '${hisonConfig.primaryColor}'. Must be a valid hex color (e.g., '#ffffff').`)
+        }
+        vgConfig.attributes.defaultGridCssInfo.color = hisonConfig.primaryColor;
+    }
+    if(hisonConfig.size) {
+        switch (hisonConfig.size) {
+            case Size.s :
+                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 3;
+                break;
+            case Size.m :
+                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 5;
+                break;
+            case Size.l :
+                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 7;
+                break;
+            case Size.xl :
+                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 9;
+                break;
+        }
+    }
+}
