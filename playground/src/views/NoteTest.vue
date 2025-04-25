674559ba6be345be59f0b4312c5dff9f71f59d93
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-25 16:54:44 +0900

    Update NoteTest.vue

--- playground/src/views/NoteTest.vue
+++ playground/src/views/NoteTest.vue
@@ -1,18 +1,21 @@
 <template>
     <h1>Test Page with multiple editors</h1>
-    <HButton @click="goBack" text-color="#ff0000">Back to Home</HButton>
-    <HButton @click="getNote1" text-color="#ff0000">getNote1</HButton>
+    <HButton @click="goBack" text-color="#ff0000">Back to Home</HButton> | 
+    <HButton @click="getNote1" text-color="#ff0000">getNote1</HButton> | 
     <HButton @click="setNote3" text-color="#ff0000">setNote3</HButton>
-    <HEditor v-model="noteData1" data-id="note1" @mounted="mountNote1" textarea-height="200px"></HEditor>
-    <HEditor v-model="noteData2" data-id="note2" @mounted="mountNote2" textarea-height="200px"></HEditor>
-    <HEditor v-model="noteData3" data-id="note3" @mounted="mountNote3" textarea-height="200px"></HEditor>
+    <br><br>
+    <HNote v-model="noteData1" data-id="note1" @mounted="mountNote1" textarea-height="200px"></HNote>
+    <br>
+    <HNote v-model="noteData2" data-id="note2" @mounted="mountNote2" textarea-height="200px"></HNote>
+    <br>
+    <HNote v-model="noteData3" data-id="note3" @mounted="mountNote3" textarea-height="200px"></HNote>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { onBeforeUpdate, onMounted, ref } from 'vue'
 import { NoteData, VanillanoteElement } from 'vanillanote2'
-import { hisonvue } from 'hisonvue'
+import { hison } from 'hisonvue'
 
 
 const router = useRouter()
@@ -24,7 +27,7 @@ const goBack = () => {
     router.push('/')
 }
 const getNote1 = () => {
-    hisonvue.utils.getNumberFormat(1234.12789,'#,##0.##')
+    hison.utils.getNumberFormat(1234.12789,'#,##0.##')
     console.log("##### getNote1");
     console.log("noteData1.value", noteData1.value);
 }
