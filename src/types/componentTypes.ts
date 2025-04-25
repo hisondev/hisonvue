41590e5d0873aa3a84056236df97fc3c422e0c0f
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-25 16:59:50 +0900

    Create componentTypes.ts

---
+++ src/types/componentTypes.ts
@@ -0,0 +1,41 @@
+import { GridAlign, GridVerticalAlign } from "../enums"
+
+
+export interface HGridColumn {
+    id: string
+    name?: string
+    header?: string
+    footer?: string
+    dataType?: string
+    untarget?: boolean
+    rowMerge?: boolean
+    colMerge?: boolean
+    visible?: boolean
+    required?: boolean
+    resizable?: boolean
+    sortable?: boolean
+    filterable?: boolean
+    width?: string
+    selectSize?: string
+    locked?: boolean
+    lockedColor?: boolean
+    format?: string
+    codes?: string
+    defaultCode?: string
+    maxLength?: number
+    maxByte?: number
+    maxNumber?: number
+    minNumber?: number
+    roundNumber?: number
+    align?: keyof typeof GridAlign
+    verticalAlign?: keyof typeof GridVerticalAlign
+    overflowWrap?: string
+    wordBreak?: string
+    whiteSpace?: string
+    backColor?: string
+    fontColor?: string
+    fontBold?: boolean
+    fontItalic?: boolean
+    fontThruline?: boolean
+    fontUnderline?: boolean
+  }
