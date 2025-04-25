be8b6e061caeef6c49c07031b44d35236b077015
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-25 16:57:32 +0900

    Create createSSRClientOnly.ts

---
+++ src/core/createSSRClientOnly.ts
@@ -0,0 +1,20 @@
+// hisonvue/utils/createSSRClientOnly.ts
+import { defineComponent, ref, h, onMounted, markRaw, Component } from 'vue'
+
+export const createSSRClientOnly = <T extends Component>(
+  importer: () => Promise<{ default: T }>,
+  componentName: string
+) =>
+defineComponent({
+  name: `${componentName}SSRWrapper`,
+  setup(_, { slots, attrs }) {
+    const comp = ref<T | null>(null)
+
+    onMounted(async () => {
+      const imported = await importer()
+      comp.value = markRaw(imported.default)
+    })
+
+    return () => comp.value ? h(comp.value, { ...attrs }, slots) : null
+  }
+})
