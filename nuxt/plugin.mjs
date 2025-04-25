1fbfa689abac7897e6ead3724efcbb6236732561
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-25 16:51:57 +0900

    Update plugin.mjs

--- nuxt/plugin.mjs
+++ nuxt/plugin.mjs
@@ -1,9 +1,9 @@
 import { defineNuxtPlugin } from '#app'
-import { hisonvueApp } from 'hisonvue'
+import { hisonvue } from 'hisonvue'
 import 'hisonvue/style.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.client) {
-    nuxtApp.vueApp.use(hisonvueApp)
+    nuxtApp.vueApp.use(hisonvue)
   }
 })
