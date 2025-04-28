7c8bdd61e4148408b34d844b36aaccd9edad82b9
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-28 16:56:52 +0900

    Create hisonCloser.ts

---
+++ src/types/hisonCloser.ts
@@ -0,0 +1,25 @@
+import { Vanillagrid } from "vanillagrid2";
+import { Vanillanote } from "vanillanote2";
+
+export interface CssEvent {
+    button_onBeforeClick: (e: MouseEvent) => boolean;
+    button_onAfterClick: (e: MouseEvent) => void;
+    button_onBeforeMouseover: (e: MouseEvent) => boolean;
+    button_onAfterMouseover: (e: MouseEvent) => void;
+    button_onBeforeMouseout: (e: MouseEvent) => boolean;
+    button_onAfterMouseout: (e: MouseEvent) => void;
+    button_onBeforeTouchstart: (e: TouchEvent) => boolean;
+    button_onAfterTouchstart: (e: TouchEvent) => void;
+    button_onBeforeTouchend: (e: TouchEvent) => boolean;
+    button_onAfterTouchend: (e: TouchEvent) => void;
+}
+
+export interface HisonvueEvent {
+    cssEvent: CssEvent;
+}
+
+export interface HisonCloser {
+    note: Vanillanote;
+    grid: Vanillagrid;
+    event: HisonvueEvent;
+}
