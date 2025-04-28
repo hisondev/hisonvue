a819816160cba28a1e5ed6d0f98429e939717be8
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-28 16:53:52 +0900

    Update HButton.vue

--- src/components/HButton/HButton.vue
+++ src/components/HButton/HButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    ref="buttonRef"
     class="hison-button"
     @click="$emit('click')"
   >
@@ -15,9 +16,10 @@
 -->
 
 <script lang="ts">
-import { defineComponent, inject, computed } from 'vue'
+import { defineComponent, inject, computed, ref, onMounted, onBeforeUnmount } from 'vue'
 import type { HisonConfig } from '../../types'
 import { Size } from "../../enums";
+import { addButtonCssEvent, removeButtonCssEvent } from '../common/setButtonEvent';
 
 export default defineComponent({
   name: 'HButton',
@@ -27,10 +29,12 @@ export default defineComponent({
   emits: ['click'],
   setup(_, { emit }) {
     const config = inject<HisonConfig>('hisonvue-config')!
+    const buttonRef = ref<HTMLButtonElement | null>(null)
+
     //속성(props)으로 받아야함!!!!!
-    const buttonColor = computed(() => config?.primaryColor || '#000')
+    const buttonColor = computed(() => config?.componentStyle.primaryColor || '#000')
     const buttonSize = computed(() => {
-      switch (config?.size) {
+      switch (config?.componentStyle.size) {
         case Size.s : return 'padding: 0.5rem 0.6rem; font-size: 0.7rem;'
         case Size.m : return 'padding: 0.6rem 0.75rem; font-size: 0.8rem;'
         case Size.l : return 'padding: 0.8rem 1rem; font-size: 0.9rem;'
@@ -39,9 +43,22 @@ export default defineComponent({
       }
     })
 
+    onMounted(() => {
+      if (buttonRef.value) {
+        addButtonCssEvent(buttonRef.value)
+      }
+    })
+
+    onBeforeUnmount(() => {
+      if (buttonRef.value) {
+        removeButtonCssEvent(buttonRef.value)
+      }
+    })
+
     return {
       buttonColor,
-      buttonSize
+      buttonSize,
+      buttonRef
     }
   }
 })
@@ -60,9 +77,9 @@ export default defineComponent({
   border-radius: 5px;
   box-shadow: 0.25px 0.25px 2px 0.25px #9dae9d;
 }
-*/
 .hison-button:hover {
   opacity: 0.7;
   box-shadow : 0.25px 0.25px 0.25px 0.25px #9dae9d;
 }
+*/
 </style>
