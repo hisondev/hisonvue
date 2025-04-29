fbeb8da89dedfaa98b5d2c5b7f93a5d0cfc2595a
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:53:54 +0900

    Update NoteTest.vue

--- playground/src/views/NoteTest.vue
+++ playground/src/views/NoteTest.vue
@@ -1,14 +1,14 @@
 <template>
     <h1>Test Page with multiple editors</h1>
-    <HButton @click="goBack" text-color="#ff0000">Back to Home</HButton> | 
-    <HButton @click="getNote1" text-color="#ff0000">getNote1</HButton> | 
-    <HButton @click="setNote3" text-color="#ff0000">setNote3</HButton>
+    <HButton @click="goBack" id="b1">Back to Home</HButton> | 
+    <HButton @click="getNote1" id="b2">getNote1</HButton> | 
+    <HButton @click="setNote3" id="b3">setNote3</HButton>
     <br><br>
-    <HNote v-model="noteData1" data-id="note1" @mounted="mountNote1" textarea-height="200px"></HNote>
+    <HNote v-model="noteData1" id="note1" @mounted="mountNote1" textarea-height="200px"></HNote>
     <br>
-    <HNote v-model="noteData2" data-id="note2" @mounted="mountNote2" textarea-height="200px"></HNote>
+    <HNote v-model="noteData2" id="note2" @mounted="mountNote2" textarea-height="200px"></HNote>
     <br>
-    <HNote v-model="noteData3" data-id="note3" @mounted="mountNote3" textarea-height="200px"></HNote>
+    <HNote v-model="noteData3" id="note3" @mounted="mountNote3" textarea-height="200px"></HNote>
 </template>
 
 <script setup lang="ts">
@@ -28,14 +28,9 @@ const goBack = () => {
 }
 const getNote1 = () => {
     hison.utils.getNumberFormat(1234.12789,'#,##0.##')
-    console.log("##### getNote1");
-    console.log("noteData1.value", noteData1.value);
 }
 const setNote3 = () => {
-    console.log("##### setNote3");
-    console.log("noteData3.value", noteData3.value);
     noteData3.value = noteData1.value;
-    console.log("noteData3.value", noteData3.value);
 }
 
 const mountNote1 = (note: VanillanoteElement) => {
@@ -46,10 +41,8 @@ const mountNote3 = () => {
 }
 
 onMounted(() => {
-    console.log("##### onMounted");
 })
 
 onBeforeUpdate(() => {
-    console.log("#### onBeforeUpdate");
 })
 </script>
