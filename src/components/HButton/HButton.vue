ca4802768ac1ddcbf3a074b2d4fd7c08f781dfac
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:54:52 +0900

    Update HButton.vue

--- src/components/HButton/HButton.vue
+++ src/components/HButton/HButton.vue
@@ -1,85 +1,73 @@
 <template>
   <button
     ref="buttonRef"
-    class="hison-button"
+    :class="['hison-button', props.addClassList, sizeClass, colorClass, visibleClass]"
+    :style="props.style"
     @click="$emit('click')"
   >
     <slot>Hison Button</slot>
   </button>
 </template>
-<!--
-  <button
-    class="hison-button"
-    :style="`background-color: ${buttonColor}; ${buttonSize}`"
-    @click="$emit('click')"
-  >
--->
 
 <script lang="ts">
 import { defineComponent, inject, computed, ref, onMounted, onBeforeUnmount } from 'vue'
 import type { HisonConfig } from '../../types'
 import { Size } from "../../enums";
 import { addButtonCssEvent, removeButtonCssEvent } from '../common/setButtonEvent';
+import { buttonProps } from './props';
+import { hisonCloser } from '../../core';
 
 export default defineComponent({
   name: 'HButton',
-  props: {
-    textColor: String
-  },
-  emits: ['click'],
-  setup(_, { emit }) {
+  props: buttonProps,
+  emits: ['mounted', 'click'],
+  setup(props, { emit }) {
     const config = inject<HisonConfig>('hisonvue-config')!
     const buttonRef = ref<HTMLButtonElement | null>(null)
+    if (!props.id) throw new Error(`[Hisonvue] button id attribute is required.`)
 
-    //속성(props)으로 받아야함!!!!!
-    const buttonColor = computed(() => config?.componentStyle.primaryColor || '#000')
-    const buttonSize = computed(() => {
-      switch (config?.componentStyle.size) {
-        case Size.s : return 'padding: 0.5rem 0.6rem; font-size: 0.7rem;'
-        case Size.m : return 'padding: 0.6rem 0.75rem; font-size: 0.8rem;'
-        case Size.l : return 'padding: 0.8rem 1rem; font-size: 0.9rem;'
-        case Size.xl : return 'padding: 1rem 1.25rem; font-size: 1rem;'
-        default: return ''
-      }
+    const sizeClass = computed(() => {
+      if (!props.size) return `hison-${config.componentStyle.size}-button`
+      return `hison-${props.size}-button`
+    })
+
+    const colorClass = computed(() => {
+      if (!props.color) return 'hison-primary-button'
+      return `hison-${props.color}-button`
+    })
+
+    const visibleClass = computed(() => {
+      if (props.visible === 'false') return 'hison-on-display-none'
+      else return ''
     })
 
     onMounted(() => {
       if (buttonRef.value) {
+        hisonCloser.element.buttonList[props.id!] = buttonRef.value
+        buttonRef.value.dataset.color = props.color || 'primary'; //이벤트를 위한 dataset처리
         addButtonCssEvent(buttonRef.value)
+
+        emit('mounted', buttonRef.value)
       }
     })
 
     onBeforeUnmount(() => {
       if (buttonRef.value) {
+        delete hisonCloser.element.buttonList[props.id!]
         removeButtonCssEvent(buttonRef.value)
       }
     })
 
     return {
-      buttonColor,
-      buttonSize,
-      buttonRef
+      buttonRef,
+      props,
+      sizeClass,
+      colorClass,
+      visibleClass
     }
   }
 })
 </script>
 
 <style scoped>
-/*
-.hison-btn {
-  padding: 0.5rem 1rem;
-  border-radius: 6px;
-  background-color: var(--primary-color, #000);
-  border: none;
-  cursor: pointer;
-  transition: opacity 0.2s;
-  margin: 2px 2px;
-  border-radius: 5px;
-  box-shadow: 0.25px 0.25px 2px 0.25px #9dae9d;
-}
-.hison-button:hover {
-  opacity: 0.7;
-  box-shadow : 0.25px 0.25px 0.25px 0.25px #9dae9d;
-}
-*/
 </style>
