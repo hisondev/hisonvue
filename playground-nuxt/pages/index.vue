ebee49b0934571a4e9eb7938825b5f06a4ce65e0
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:52:50 +0900

    Update index.vue

--- playground-nuxt/pages/index.vue
+++ playground-nuxt/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <h1>Nuxt Home Page</h1>
-    <HButton @click="goToGridTest" text-color="#007bff">Go to Grid Test Page</HButton><br><br>
-    <HButton @click="goToNoteTest" text-color="#007bff">Go to Note Test Page</HButton>
+    <HButton @click="goToGridTest" id="b1">Go to Grid Test Page</HButton><br><br>
+    <HButton @click="goToNoteTest" id="b2">Go to Note Test Page</HButton>
 </template>
 
 <script setup lang="ts">
