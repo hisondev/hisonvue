a29da33090f45b5f64766e6e93bd7b6368c29320
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:58:21 +0900

    Update hisonCloser.ts

--- src/types/hisonCloser.ts
+++ src/types/hisonCloser.ts
@@ -1,25 +1,29 @@
-import { Vanillagrid } from "vanillagrid2";
-import { Vanillanote } from "vanillanote2";
+import { Vanillagrid } from "vanillagrid2"
+import { Vanillanote } from "vanillanote2"
+import { HButton } from "./component"
 
 export interface CssEvent {
-    button_onBeforeClick: (e: MouseEvent) => boolean;
-    button_onAfterClick: (e: MouseEvent) => void;
-    button_onBeforeMouseover: (e: MouseEvent) => boolean;
-    button_onAfterMouseover: (e: MouseEvent) => void;
-    button_onBeforeMouseout: (e: MouseEvent) => boolean;
-    button_onAfterMouseout: (e: MouseEvent) => void;
-    button_onBeforeTouchstart: (e: TouchEvent) => boolean;
-    button_onAfterTouchstart: (e: TouchEvent) => void;
-    button_onBeforeTouchend: (e: TouchEvent) => boolean;
-    button_onAfterTouchend: (e: TouchEvent) => void;
+    button_onBeforeClick: (e: MouseEvent) => boolean
+    button_onAfterClick: (e: MouseEvent) => void
+    button_onBeforeMouseover: (e: MouseEvent) => boolean
+    button_onAfterMouseover: (e: MouseEvent) => void
+    button_onBeforeMouseout: (e: MouseEvent) => boolean
+    button_onAfterMouseout: (e: MouseEvent) => void
+    button_onBeforeTouchstart: (e: TouchEvent) => boolean
+    button_onAfterTouchstart: (e: TouchEvent) => void
+    button_onBeforeTouchend: (e: TouchEvent) => boolean
+    button_onAfterTouchend: (e: TouchEvent) => void
 }
 
 export interface HisonvueEvent {
-    cssEvent: CssEvent;
+    cssEvent: CssEvent
 }
 
 export interface HisonCloser {
-    note: Vanillanote;
-    grid: Vanillagrid;
-    event: HisonvueEvent;
+    note: Vanillanote
+    grid: Vanillagrid
+    element: {
+        buttonList: Record<string, HButton>
+    }
+    event: HisonvueEvent
 }
