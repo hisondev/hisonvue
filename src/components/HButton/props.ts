abbe307569de65c60366dda2deabe13b220f0443
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:55:05 +0900

    Create props.ts

---
+++ src/components/HButton/props.ts
@@ -0,0 +1,12 @@
+import { PropType } from "vue";
+import { Color, Size } from "../../enums";
+import { BoolString } from "../../enums/props";
+
+export const buttonProps = {
+    id: { type: String, required: true },
+    addClassList: { type: String, required: false },
+    style: { type: Object as PropType<Record<string, string>>, required: false },
+    size: { type: String as PropType<Size>, required: false },
+    color: { type: String as PropType<Color>, required: false },
+    visible: { type: String as PropType<BoolString>, required: false },
+}
