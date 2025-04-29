4be5008f61b3c12bdb490e572ee0676833fa0625
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-29 16:55:48 +0900

    Update props.ts

--- src/components/HNote/props.ts
+++ src/components/HNote/props.ts
@@ -1,10 +1,11 @@
 import { PropType } from "vue";
-import { NoteModeByDevice, NoteToolPosition } from "../../enums";
+import { Color, NoteModeByDevice, NoteToolPosition } from "../../enums";
 import { NoteData } from "vanillanote2";
+import { BoolString } from "../../enums/props";
 
 export const noteProps = {
     modelValue: Object as () => NoteData,
-    dataId: { type: String, required: true },
+    id: { type: String, required: true },
     /**
      * ADAPTIVE : The composition of notes varies depending on whether the device is a desktop or mobile device.
      * MOBILE : Always recognize as mobile.
@@ -22,7 +23,7 @@ export const noteProps = {
     /**
     * Whether to use the toolbar fold/unfold function
     */
-    toolToggle: { type: String, required: false },
+    toolToggle: { type: String as PropType<BoolString>, required: false },
     /**
      * - Variables for dynamically setting the size of the textarea
      * - Default width of the textarea. If not set, the value of textarea-width is inserted. Used for dynamically changing the width.
@@ -46,13 +47,13 @@ export const noteProps = {
     /**
      * Whether the user can change the height of the textarea. If true, it can be changed.
      */
-    textareaHeightIsModify: { type: String, required: false },
+    textareaHeightIsModify: { type: String as PropType<BoolString>, required: false },
     /**
      * - Values related to placeholders. The attribute placeholder- can be used, but using these variables allows dynamic control of placeholders.
      * - `true` : Uses a placeholder.
      * - `false` : Default value. Does not use a placeholder.
      */
-    placeholderIsVisible: { type: String, required: false },
+    placeholderIsVisible: { type: String as PropType<BoolString>, required: false },
     /**
      * - Values related to placeholders. The attribute placeholder- can be used, but using these variables allows dynamic control of placeholders.
      * - Adjusts the vertical position of the placeholder. Negative values are possible. The unit is px. Default value is 0.
@@ -174,6 +175,10 @@ export const noteProps = {
     /** 
      * Main theme color for the editor
      */
+    color: { type: String as PropType<Color>, required: false },
+    /** 
+     * Main theme color for the editor
+     */
     mainColor: { type: String, required: false },
     /** 
      * Predefined color set name
@@ -182,109 +187,109 @@ export const noteProps = {
     /** 
      * Whether to use inverted (dark mode) colors
      */
-    invertColor: { type: String, required: false },
+    invertColor: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use paragraph style buttons (normal, heading, etc.)
      */
-    usingParagraphStyle: { type: String, required: false },
+    usingParagraphStyle: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use bold button
      */
-    usingBold: { type: String, required: false },
+    usingBold: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use underline button
      */
-    usingUnderline: { type: String, required: false },
+    usingUnderline: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use italic button
      */
-    usingItalic: { type: String, required: false },
+    usingItalic: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use unordered list (ul) button
      */
-    usingUl: { type: String, required: false },
+    usingUl: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use ordered list (ol) button
      */
-    usingOl: { type: String, required: false },
+    usingOl: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use text align buttons (left, center, right)
      */
-    usingTextAlign: { type: String, required: false },
+    usingTextAlign: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use attach link button
      */
-    usingAttLink: { type: String, required: false },
+    usingAttLink: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use attach file button
      */
-    usingAttFile: { type: String, required: false },
+    usingAttFile: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use attach image button
      */
-    usingAttImage: { type: String, required: false },
+    usingAttImage: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use attach video button
      */
-    usingAttVideo: { type: String, required: false },
+    usingAttVideo: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow font size adjustment
      */
-    usingFontSize: { type: String, required: false },
+    usingFontSize: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow letter spacing adjustment
      */
-    usingLetterSpacing: { type: String, required: false },
+    usingLetterSpacing: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow line height adjustment
      */
-    usingLineHeight: { type: String, required: false },
+    usingLineHeight: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow font family change
      */
-    usingFontFamily: { type: String, required: false },
+    usingFontFamily: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow text color change
      */
-    usingColorText: { type: String, required: false },
+    usingColorText: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to allow background color change
      */
-    usingColorBack: { type: String, required: false },
+    usingColorBack: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use format clear (remove styles) button
      */
-    usingFormatClear: { type: String, required: false },
+    usingFormatClear: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use undo button
      */
-    usingUndo: { type: String, required: false },
+    usingUndo: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use redo button
      */
-    usingRedo: { type: String, required: false },
+    usingRedo: { type: String as PropType<BoolString>, required: false },
     /** 
      * Whether to use help (shortcut guide) button
      */
-    usingHelp: { type: String, required: false },
+    usingHelp: { type: String as PropType<BoolString>, required: false },
     /**
      * Whether to use paragraph style all buttons
      */
-    usingParagraphAllStyle: { type: String, required: false },
+    usingParagraphAllStyle: { type: String as PropType<BoolString>, required: false },
     /**
      * Whether to use character style all buttons
      */
-    usingCharacterStyle: { type: String, required: false },
+    usingCharacterStyle: { type: String as PropType<BoolString>, required: false },
     /**
      * Whether to use character size all buttons
      */
-    usingCharacterSize: { type: String, required: false },
+    usingCharacterSize: { type: String as PropType<BoolString>, required: false },
     /**
      * Whether to use attachment all buttons
      */
-    usingAttachFile: { type: String, required: false },
+    usingAttachFile: { type: String as PropType<BoolString>, required: false },
     /**
      * Whether to use redo and undo buttons
      */
-    usingDo: { type: String, required: false },
+    usingDo: { type: String as PropType<BoolString>, required: false },
 }
