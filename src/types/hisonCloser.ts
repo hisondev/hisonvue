e7dfbd2ab75dc6c1fcddf55be13ae56e580c81e0
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-30 16:54:26 +0900

    Update hisonCloser.ts

--- src/types/hisonCloser.ts
+++ src/types/hisonCloser.ts
@@ -1,29 +1,75 @@
 import { Vanillagrid } from "vanillagrid2"
 import { Vanillanote } from "vanillanote2"
 import { HButton } from "./component"
+import { ComponentStyleHisonConfig, HisonvueEvent } from "./hisonConfig"
 
-export interface CssEvent {
-    button_onBeforeClick: (e: MouseEvent) => boolean
-    button_onAfterClick: (e: MouseEvent) => void
-    button_onBeforeMouseover: (e: MouseEvent) => boolean
-    button_onAfterMouseover: (e: MouseEvent) => void
-    button_onBeforeMouseout: (e: MouseEvent) => boolean
-    button_onAfterMouseout: (e: MouseEvent) => void
-    button_onBeforeTouchstart: (e: TouchEvent) => boolean
-    button_onAfterTouchstart: (e: TouchEvent) => void
-    button_onBeforeTouchend: (e: TouchEvent) => boolean
-    button_onAfterTouchend: (e: TouchEvent) => void
+export interface ComponentColor {
+  primary: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
+  muted: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
+  info: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
+  success: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
+  danger: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
+  warning: {
+    buttonColor: string | null;
+    borderColor: string | null;
+    shadowColor: string | null;
+    hoverColor: string | null;
+    activeColor: string | null;
+    emptyTextColor: string | null;
+    filledTextColor: string | null;
+  }
 }
 
-export interface HisonvueEvent {
-    cssEvent: CssEvent
+export interface ComponentStyle extends ComponentStyleHisonConfig{
+  componentColor: ComponentColor
 }
 
 export interface HisonCloser {
     note: Vanillanote
     grid: Vanillagrid
-    element: {
+    component: {
         buttonList: Record<string, HButton>
     }
     event: HisonvueEvent
+    componentStyle: ComponentStyle
 }
