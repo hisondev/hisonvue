fd85d14d5347191ee9653993d58b7a30ac2b070c
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-30 16:52:58 +0900

    Create setHison.ts

---
+++ src/core/setHison.ts
@@ -0,0 +1,91 @@
+import { setHisonFromHisonConfig } from "../plugins";
+import { Hison, HisonConfig } from "../types";
+import { applyCssVariables } from "./setDocumentFromHisonCloser";
+import { hisonCloser } from "./createHisonCloser";
+import { Size } from "../enums";
+import { reloadAllHisonComponents } from "../utils/";
+
+export const setHison = (hison: Hison, hisonConfig: HisonConfig) => {
+    setHisonFromHisonConfig(hison, hisonConfig)
+  
+    hison.cssEvent = {
+      setButtonOnBefoerClick(func: ((e: MouseEvent) => boolean)) {hisonCloser.event.cssEvent.button_onBeforeClick = func},
+      setButtonOnAfterClick(func: ((e: MouseEvent) => void)) {hisonCloser.event.cssEvent.button_onAfterClick = func},
+      setButtonOnBeforeMouseover(func: ((e: MouseEvent) => boolean)) {hisonCloser.event.cssEvent.button_onBeforeMouseover = func},
+      setButtonOnAfterMouseover(func: ((e: MouseEvent) => void)) {hisonCloser.event.cssEvent.button_onAfterMouseover = func},
+      setButtonOnBeforeMouseout(func: ((e: MouseEvent) => boolean)) {hisonCloser.event.cssEvent.button_onBeforeMouseout = func},
+      setButtonOnAfterMouseout(func: ((e: MouseEvent) => void)) {hisonCloser.event.cssEvent.button_onAfterMouseout = func},
+      setButtonOnBeforeTouchstart(func: ((e: TouchEvent) => boolean)) {hisonCloser.event.cssEvent.button_onBeforeTouchstart = func},
+      setButtonOnAfterTouchstart(func: ((e: TouchEvent) => void)) {hisonCloser.event.cssEvent.button_onAfterTouchstart = func},
+      setButtonOnBeforeTouchend(func: ((e: TouchEvent) => boolean)) {hisonCloser.event.cssEvent.button_onBeforeTouchend = func},
+      setButtonOnAfterTouchend(func: ((e: TouchEvent) => void)) {hisonCloser.event.cssEvent.button_onAfterTouchend = func}
+    };
+
+    const reApplyCssVariables = () => {
+        if (typeof window !== 'undefined') {
+          applyCssVariables()
+          reloadAllHisonComponents()
+        }
+      }
+
+    hison.style = {
+        setSize(size: Size.s | Size.m | Size.l | Size.xl) {
+            hisonCloser.componentStyle.size = size;
+            reApplyCssVariables()
+        },
+        setFilledColor(filledColor: string) {
+            hisonCloser.componentStyle.filledColor = filledColor;
+            reApplyCssVariables()
+        },
+        setEmptyColor(emptyColor: string) {
+            hisonCloser.componentStyle.emptyColor = emptyColor;
+            reApplyCssVariables()
+        },
+        setFilledTextColor(filledTextColor: string) {
+            hisonCloser.componentStyle.filledTextColor = filledTextColor;
+            reApplyCssVariables()
+        },
+        setEmptyTextColor(emptyTextColor: string) {
+            hisonCloser.componentStyle.emptyTextColor = emptyTextColor;
+            reApplyCssVariables()
+        },
+        setPrimaryColor(primaryColor: string) {
+            hisonCloser.componentStyle.primaryColor = primaryColor;
+            reApplyCssVariables()
+        },
+        setMutedColor(mutedColor: string) {
+            hisonCloser.componentStyle.mutedColor = mutedColor;
+            reApplyCssVariables()
+        },
+        setInfoColor(infoColor: string) {
+            hisonCloser.componentStyle.infoColor = infoColor;
+            reApplyCssVariables()
+        },
+        setSuccessColor(successColor: string) {
+            hisonCloser.componentStyle.successColor = successColor;
+            reApplyCssVariables()
+        },
+        setDangerColor(dangerColor: string) {
+            hisonCloser.componentStyle.dangerColor = dangerColor;
+            reApplyCssVariables()
+        },
+        setWarningColor(warningColor: string) {
+            hisonCloser.componentStyle.warningColor = warningColor;
+            reApplyCssVariables()
+        },
+        setInvertColor(invert: boolean) {
+            hisonCloser.componentStyle.invertColor = invert;
+            reApplyCssVariables()
+        },
+    };
+    
+    (hison as any).vue = {}
+    hison.vue.getNote = (noteId: string) => { return hisonCloser.note.getNote(noteId) }
+    hison.vue.getGrid = (gridId: string) => { return hisonCloser.grid.getGrid(gridId) }
+    hison.vue.getButton = (buttonId: string) => { return hisonCloser.component.buttonList[buttonId] }
+  
+    //window setting
+    if (typeof window !== 'undefined') {
+      applyCssVariables()
+    }
+  }
