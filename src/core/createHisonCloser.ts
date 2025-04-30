37e639415fba34c28540a09f630862088cf0e5d4
hisondev <64723261+hisondev@users.noreply.github.com>
2025-04-30 16:51:57 +0900

    Update and rename setHisonCloserFromHisonConfig.ts to createHisonCloser.ts

---
+++ src/core/createHisonCloser.ts
@@ -0,0 +1,128 @@
+import { getVg, getVn } from "../plugins"
+import { HisonConfig, HisonCloser } from "../types"
+import { applyDefaultColor, colorCycleChange, getBasicTextColor, normalizeToRgba } from "../utils"
+
+export const hisonCloser = {
+  event : {
+    cssEvent: {
+      button_onBeforeClick: (e: MouseEvent) => { return true },
+      button_onAfterClick: (e: MouseEvent) => {},
+      button_onBeforeMouseover: (e: MouseEvent) => { return true },
+      button_onAfterMouseover: (e: MouseEvent) => {},
+      button_onBeforeMouseout: (e: MouseEvent) => { return true },
+      button_onAfterMouseout: (e: MouseEvent) => {},
+      button_onBeforeTouchstart: (e: TouchEvent) => { return true },
+      button_onAfterTouchstart: (e: TouchEvent) => {},
+      button_onBeforeTouchend: (e: TouchEvent) => { return true },
+      button_onAfterTouchend: (e: TouchEvent) => {},
+    }
+  }
+} as HisonCloser
+
+export const createHisonCloser = (hisonConfig: HisonConfig) => {
+  // 이벤트 설정
+  if(hisonConfig.event.cssEvent.button_onBeforeClick) hisonCloser.event.cssEvent.button_onBeforeClick = hisonConfig.event.cssEvent.button_onBeforeClick
+  if(hisonConfig.event.cssEvent.button_onAfterClick) hisonCloser.event.cssEvent.button_onAfterClick = hisonConfig.event.cssEvent.button_onAfterClick
+  if(hisonConfig.event.cssEvent.button_onBeforeMouseover) hisonCloser.event.cssEvent.button_onBeforeMouseover = hisonConfig.event.cssEvent.button_onBeforeMouseover
+  if(hisonConfig.event.cssEvent.button_onAfterMouseover) hisonCloser.event.cssEvent.button_onAfterMouseover = hisonConfig.event.cssEvent.button_onAfterMouseover
+  if(hisonConfig.event.cssEvent.button_onBeforeMouseout) hisonCloser.event.cssEvent.button_onBeforeMouseout = hisonConfig.event.cssEvent.button_onBeforeMouseout
+  if(hisonConfig.event.cssEvent.button_onAfterMouseout) hisonCloser.event.cssEvent.button_onAfterMouseout = hisonConfig.event.cssEvent.button_onAfterMouseout
+  if(hisonConfig.event.cssEvent.button_onBeforeTouchstart) hisonCloser.event.cssEvent.button_onBeforeTouchstart = hisonConfig.event.cssEvent.button_onBeforeTouchstart
+  if(hisonConfig.event.cssEvent.button_onAfterTouchstart) hisonCloser.event.cssEvent.button_onAfterTouchstart = hisonConfig.event.cssEvent.button_onAfterTouchstart
+  if(hisonConfig.event.cssEvent.button_onBeforeTouchend) hisonCloser.event.cssEvent.button_onBeforeTouchend = hisonConfig.event.cssEvent.button_onBeforeTouchend
+  if(hisonConfig.event.cssEvent.button_onAfterTouchend) hisonCloser.event.cssEvent.button_onAfterTouchend = hisonConfig.event.cssEvent.button_onAfterTouchend
+
+  //컴포넌트 스타일 설정
+  hisonCloser.componentStyle = {
+    size : hisonConfig.componentStyle.size,
+    filledColor : hisonConfig.componentStyle.filledColor,
+    emptyColor : hisonConfig.componentStyle.emptyColor,
+    filledTextColor : hisonConfig.componentStyle.filledTextColor,
+    emptyTextColor : hisonConfig.componentStyle.emptyTextColor,
+    primaryColor : hisonConfig.componentStyle.primaryColor,
+    mutedColor : hisonConfig.componentStyle.mutedColor,
+    infoColor : hisonConfig.componentStyle.infoColor,
+    successColor : hisonConfig.componentStyle.successColor,
+    dangerColor : hisonConfig.componentStyle.dangerColor,
+    warningColor : hisonConfig.componentStyle.warningColor,
+    invertColor : hisonConfig.componentStyle.invertColor,
+    componentColor : {
+      primary: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      },
+      muted: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      },
+      info: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      },
+      success: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      },
+      danger: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      },
+      warning: {
+        buttonColor : null,
+        borderColor : null,
+        shadowColor : null,
+        hoverColor : null,
+        activeColor : null,
+        emptyTextColor : null,
+        filledTextColor : null,
+      }
+    }
+  }
+  const c = hisonCloser.componentStyle;
+  colorCycleChange(c, normalizeToRgba);
+  if (!c.filledTextColor) c.filledTextColor = getBasicTextColor(c.filledColor);
+  if (!c.emptyTextColor) c.emptyTextColor = getBasicTextColor(c.emptyColor);
+  const cc = c.componentColor;
+  applyDefaultColor(cc.primary, c.primaryColor);
+  applyDefaultColor(cc.muted, c.mutedColor);
+  applyDefaultColor(cc.info, c.infoColor);
+  applyDefaultColor(cc.success, c.successColor);
+  applyDefaultColor(cc.danger, c.dangerColor);
+  applyDefaultColor(cc.warning, c.warningColor);
+  
+  // plugin설정
+  const vn = getVn()
+  const vg = getVg()
+  hisonCloser.note = vn
+  hisonCloser.grid = vg
+
+  // 컴포넌트 리스트 생성
+  hisonCloser.component = {
+    buttonList: {}
+  }
+}
