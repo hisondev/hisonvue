293af63c7d9375ce9791a3263106208878f73c3b
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-02 16:34:33 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -7,11 +7,9 @@ import router from './router'
 const app = createApp(App)
 
 const hisonConfig: HisonConfig = getDefaultHisonConfig()
-console.log('33333', hisonConfig.componentStyle.primaryColor)
 hisonConfig.componentStyle.primaryColor = '#217346'
 hisonConfig.componentStyle.size = Size.s
 hisonConfig.event.cssEvent.button_onBeforeClick = (e: MouseEvent) => {console.log('button_onBeforeClick');return true}
-console.log('44444', hisonConfig.componentStyle.primaryColor)
 
 app.use(hisonvue, hisonConfig)
 app.use(router)
