93a3f64b5fc702ff0928cf72960ee7eeb25e4974
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-02 16:37:12 +0900

    Update HButtonInner.vue

--- src/components/HButton/HButtonInner.vue
+++ src/components/HButton/HButtonInner.vue
@@ -14,23 +14,23 @@
         :style="props.style"
         :title="props.title || undefined"
         :disabled="props.disable === 'true'"
-        @click="handleClick"
-        @mousedown="handleMousedown"
-        @mouseup="handleMouseup"
-        @mouseover="handleMouseover"
-        @mouseout="handleMouseout"
+        @click="$emit('click', $event)"
+        @mousedown="$emit('mousedown', $event)"
+        @mouseup="$emit('mouseup', $event)"
+        @mouseover="$emit('mouseover', $event)"
+        @mouseout="$emit('mouseout', $event)"
     >
         <slot>Hison Button</slot>
     </button>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, computed, ref, onMounted, onBeforeUnmount } from 'vue'
-import type { HButton, HisonConfig } from '../../types'
+import { defineComponent, computed, ref, onMounted, onBeforeUnmount } from 'vue'
+import type { HButtonMethods } from '../../types'
 import { Size } from "../../enums"
 import { addButtonCssEvent, removeButtonCssEvent } from '../common/setButtonEvent'
 import { buttonProps } from './props'
-import { hisonCloser } from '../../core'
+import { hisonCloser } from '../..'
 
 export default defineComponent({
 name: 'HButtonInner',
@@ -38,12 +38,10 @@ props: buttonProps,
 inheritAttrs: false,
 emits: ['mounted', 'click', 'mousedown', 'mouseup', 'mouseover', 'mouseout'],
 setup(props, { emit, attrs }) {
-    const config = inject<HisonConfig>('hisonvue-config')!
     const buttonRef = ref<HTMLButtonElement | null>(null)
 
     if (!props.id) throw new Error(`[Hisonvue] button id attribute is required.`)
-
-    const sizeClass = computed(() => `hison-${props.size || config.componentStyle.size}-button`)
+    const sizeClass = computed(() => `hison-${props.size || hisonCloser.componentStyle.size}-button`)
     const colorClass = computed(() => `hison-${props.color || 'primary'}-button`)
     const visibleClass = computed(() => props.visible === 'false' ? 'hison-display-none' : '')
     const positionClass = computed(() => {
@@ -53,52 +51,29 @@ setup(props, { emit, attrs }) {
     })
     const disableClass = computed(() => props.disable === 'true' ? 'hison-disable' : '')
 
-    const handleClick = (e: MouseEvent) => {
-        if (props.disable === 'true') return
-        emit('click', e)
-    }
-    const handleMousedown = (e: MouseEvent) => {
-        if (props.disable === 'true') return
-        emit('mousedown', e)
-    }
-    const handleMouseup = (e: MouseEvent) => {
-        if (props.disable === 'true') return
-        emit('mouseup', e)
-    }
-    const handleMouseover = (e: MouseEvent) => {
-        if (props.disable === 'true') return
-        emit('mouseover', e)
-    }
-    const handleMouseout = (e: MouseEvent) => {
-        if (props.disable === 'true') return
-        emit('mouseout', e)
-    }
-
-    const setMethods = (element: HButton) => {
-        element.isDisable = () => element.disabled
-        element.isVisible = () => window.getComputedStyle(element).display !== 'none'
-        element.setDisable = (val: boolean) => {
-            element.disabled = val
-            element.classList.toggle('hison-disable', val)
-        }
-        element.setVisible = (val: boolean) => {
-            element.classList.toggle('hison-display-none', !val)
-        }
-    }
-
+    
     onMounted(() => {
-        console.log('buttonRef onMounted')
         if (buttonRef.value) {
-            setMethods(buttonRef.value as HButton)
-            hisonCloser.component.buttonList[props.id!] = buttonRef.value as HButton
             buttonRef.value.dataset.color = props.color || 'primary'
             addButtonCssEvent(buttonRef.value)
-            emit('mounted', buttonRef.value)
+            //add methods
+            const buttonMethods: HButtonMethods = {
+                isDisable : () => buttonRef.value!.disabled,
+                isVisible : () => window.getComputedStyle(buttonRef.value!).display !== 'none',
+                setDisable : (val: boolean) => {
+                    buttonRef.value!.disabled = val
+                    buttonRef.value!.classList.toggle('hison-disable', val)
+                },
+                setVisible : (val: boolean) => {
+                    buttonRef.value!.classList.toggle('hison-display-none', !val)
+                },
+            }
+            hisonCloser.component.buttonList[props.id!] = buttonMethods
+            emit('mounted', buttonMethods)
         }
     })
 
     onBeforeUnmount(() => {
-        console.log('buttonRef onBeforeUnmount')
         if (buttonRef.value) {
             delete hisonCloser.component.buttonList[props.id!]
             removeButtonCssEvent(buttonRef.value)
@@ -114,11 +89,6 @@ setup(props, { emit, attrs }) {
         visibleClass,
         positionClass,
         disableClass,
-        handleClick,
-        handleMousedown,
-        handleMouseup,
-        handleMouseover,
-        handleMouseout
     }
 }
 })
