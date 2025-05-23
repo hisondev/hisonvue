a2c12cdac604d6e3d5124f50387aab48d8c815a6
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:51:14 +0900

    Update GridTest.vue

--- playground/src/views/GridTest.vue
+++ playground/src/views/GridTest.vue
@@ -7,16 +7,18 @@
   <HGrid
     id="grid01"
     :columns="columns"
+    class="hison-col-4"
     :height="'200px'"
     :rownumVisible="'false'"
     :statusVisible="'false'"
     :visible="'true'"
-    color="black"
+    :activeRow="onActiveRow"
+    :activeCol="onActiveCol"
     @mounted="mountGrid1"
   />
-  <br>
   <HGrid
     id="grid02"
+    class="hison-col-4"
     :columns="columns"
     :height="'200px'"
     :rownumVisible="'false'"
@@ -24,15 +26,28 @@
     :color="'danger'"
     @mounted="mountGrid2"
   />
-  <br>
   <HGrid
     id="grid03"
+    class="hison-col-4"
     :columns="columns"
     :height="'200px'"
     :rownumVisible="'false'"
     :statusVisible="'false'"
     :color="'warning'"
+    :activeCell="onActiveCell"
+    :activeCells="onActiveCells"
     @mounted="mountGrid3"
+  />
+  <HGrid
+    id="grid04"
+    class="
+    hison-col-12-mb hison-col-9-tb hison-col-6-pc hison-col-3-wd
+    hison-color-muted-mb hison-color-info-tb hison-color-success-pc hison-color-warning-wd
+    hison-size-xl-mb hison-size-l-tb hison-size-m-pc hison-size-s-wd
+    "
+    :columns="testColumns"
+    :height="'400px'"
+    @mounted="mountGridTest"
   />
 </template>
 
@@ -41,6 +56,11 @@
   import { hison, type HGridColumn, GridAlign } from 'hisonvue'
   import type { GridMethods } from 'vanillagrid2'
 
+  const onActiveRow = (row: number) => {console.log('onActiveRow',row)}
+  const onActiveCol = (colId: string) => {console.log('onActiveCol',colId)}
+  const onActiveCell = (row: number, colId: string) => {console.log('onActiveCell',row, colId)}
+  const onActiveCells = (startRow: number, startColId: string, endRow: number, endColId: string) => {console.log('onActiveCells',startRow,startColId,endRow,endColId)}
+
   // 라우터
   const router = useRouter()
   const goBack = () => router.push('/')
@@ -48,42 +68,224 @@
   // 컬럼 정의
   const columns: HGridColumn[] = [
     { id: 'col1', header: 'header;1', dataType: 'text', width: '25%' },
-    { id: 'col2', header: ';2', dataType: 'text', width: '25%' },
-    { id: 'col3', header: ';3', dataType: 'text', width: '25%' },
+    { id: 'col2', header: ';2', dataType: 'number', width: '25%' },
+    { id: 'col3', header: ';3', dataType: 'button', width: '25%' },
     { id: 'col4', header: ';4', dataType: 'text', width: '25%', align : GridAlign.right,}
   ]
 
+  const testColumns: HGridColumn[] = [
+    { id : "dept", header : "Click the Sigma button to the left of the header;filter1(mask);dept ", dataType : "mask", format : "AAA99", width : "100", rowMerge : true },
+    { id : "f_nm", header : ";filter2(text);first name", dataType : "text", width : "120", align : "center", locked : true },
+    { id : "l_nm", header : ";filter3(text);last name", dataType : "text", width : "120", align : "center", locked : true, colMerge : true },
+    { id : "d_o_j", header : ";filter4(month);DOJ", dataType : "month", format : "yyyy/mm", width : "120", align : "center", locked : true },
+    { id : "e_id", header : "sort1(text);;", dataType : "number", width : "120", align : "center", locked : true, footer : "MAX;MIN;AVG;SUM", roundNumber : -1 },
+    { id : "salary", header : "sort2(number);;salary", dataType : "number", format : "$ #,###.#####", width : "150", footer : "$$MAX;$$MIN;$$AVG;$$SUM" },
+    { id : "status", header : "Please double click;checkbox;status", dataType : "checkbox", width : "80", align : "center", footer : "$$COUNT_CHECK" },
+    { id : "radio", header : "radio", dataType : "radio", width : "80", align : "center", footer : "$$COUNT_CHECK" }
+  ]
+
   let sampleData = [
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' }
+      { col1: 'A', col2: '2', col3: 'C', col4: 'D' },
+      { col1: 'A', col2: '3', col3: 'C', col4: 'D' },
+      { col1: 'A', col2: '4', col3: 'C', col4: 'D' },
+      { col1: 'A', col2: '5', col3: 'C', col4: 'D' },
+      { col1: 'A', col2: '6', col3: 'C', col4: 'D' }
     ];
 
+
+  let invertColorToggle = true
   // 버튼 테스트용
   const test1 = () => {
-    const grid1 = hison.vue.getGrid("grid01")!;
-    sampleData = [
-      { col1: 'Z', col2: 'Y', col3: 'X', col4: 'W' },
-      { col1: 'Z', col2: 'Y', col3: 'X', col4: 'W' },
-      { col1: 'Z', col2: 'Y', col3: 'X', col4: 'W' },
-      { col1: 'Z', col2: 'Y', col3: 'X', col4: 'W' },
-      { col1: 'Z', col2: 'Y', col3: 'X', col4: 'W' }
-    ];
-    hison.style.setInvertColor(true)
+    const grid4 = hison.vue.getGrid("grid04")!;
+    grid4.load(testData)
   }
   const test2 = () => {
-    const grid2 = hison.vue.getGrid("grid02")!;
-    sampleData = [
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' },
-      { col1: 'A', col2: 'B', col3: 'C', col4: 'D' }
-    ];
-    hison.style.setInvertColor(false)
+    hison.style.setInvertColor(invertColorToggle)
+    invertColorToggle = !invertColorToggle
   }
+
+  const testData = [
+    {
+        dept : "AAA01",
+        f_nm : "James",
+        l_nm : "Smith",
+        d_o_j : "201603",
+        e_id : "7",
+        salary : 100000,
+        status : "Y",
+    },
+    {
+        dept : "AAA01",
+        f_nm : "Johnson",
+        l_nm : "Johnson",
+        d_o_j : "200103",
+        e_id : "2",
+        salary : 200000,
+        status : "Y",
+    },
+    {
+        dept : "AAA01",
+        f_nm : "Michael",
+        l_nm : "Williams",
+        d_o_j : "200803",
+        e_id : "4",
+        salary : 150000,
+        status : "N",
+    },
+    {
+        dept : "AAA01",
+        f_nm : "Smith",
+        l_nm : "Smith",
+        d_o_j : "202403",
+        e_id : "46",
+        salary : 50000,
+        status : "Y",
+    },
+    {
+        dept : "AAA01",
+        f_nm : "William",
+        l_nm : "Brown",
+        d_o_j : "202203",
+        e_id : "25",
+        salary : 70000,
+        status : "N",
+    },
+    {
+        dept : "AAA01",
+        f_nm : "Charles",
+        l_nm : "Jones",
+        d_o_j : "202103",
+        e_id : "27",
+        salary : 30000,
+        status : "N",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "David",
+        l_nm : "Smith",
+        d_o_j : "201403",
+        e_id : "12",
+        salary : 180000,
+        status : "N",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "James",
+        l_nm : "Jones",
+        d_o_j : "202303",
+        e_id : "26",
+        salary : 80000,
+        status : "N",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "Richard",
+        l_nm : "Garcia",
+        d_o_j : "202203",
+        e_id : "24",
+        salary : 60000,
+        status : "Y",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "David",
+        l_nm : "Miller",
+        d_o_j : "202403",
+        e_id : "48",
+        salary : 90000,
+        status : "Y",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "Joseph",
+        l_nm : "Davis",
+        d_o_j : "202403",
+        e_id : "34",
+        salary : 40000,
+        status : "Y",
+    },
+    {
+        dept : "AAA02",
+        f_nm : "Richard",
+        l_nm : "Miller",
+        d_o_j : "202303",
+        e_id : "35",
+        salary : 50000,
+        status : "Y",
+    },
+    {
+        dept : "BBB01",
+        f_nm : "Joseph",
+        l_nm : "Davis",
+        d_o_j : "200403",
+        e_id : "3",
+        salary : 140000,
+        status : "Y",
+    },
+    {
+        dept : "BBB01",
+        f_nm : "Thomas",
+        l_nm : "Johnson",
+        d_o_j : "200903",
+        e_id : "9",
+        salary : 110000,
+        status : "N",
+    },
+    {
+        dept : "BBB01",
+        f_nm : "Charles",
+        l_nm : "Brown",
+        d_o_j : "200703",
+        e_id : "11",
+        salary : 130000,
+        status : "Y",
+    },
+    {
+        dept : "BBB01",
+        f_nm : "James",
+        l_nm : "Rodriguez",
+        d_o_j : "201303",
+        e_id : "14",
+        salary : 120000,
+        status : "N",
+    },
+    {
+        dept : "BBB02",
+        f_nm : "Richard",
+        l_nm : "Garcia",
+        d_o_j : "200803",
+        e_id : "10",
+        salary : 190000,
+        status : "Y",
+    },
+    {
+        dept : "BBB02",
+        f_nm : "David",
+        l_nm : "Martinez",
+        d_o_j : "202403",
+        e_id : "37",
+        salary : 60000,
+        status : "Y",
+    },
+    {
+        dept : "BBB02",
+        f_nm : "Robert",
+        l_nm : "Rodriguez",
+        d_o_j : "202303",
+        e_id : "31",
+        salary : 90000,
+        status : "N",
+    },
+    {
+        dept : "BBB02",
+        f_nm : "James",
+        l_nm : "Williams",
+        d_o_j : "202403",
+        e_id : "40",
+        salary : 60000,
+        status : "Y",
+    },
+  ];
 
   // 각 grid mount 이벤트
   const mountGrid1 = (grid: GridMethods) => {
@@ -94,5 +296,7 @@
   }
   const mountGrid3 = (grid: GridMethods) => {
     grid.load(sampleData)
+  }
+  const mountGridTest = (grid: GridMethods) => {
   }
 </script>
