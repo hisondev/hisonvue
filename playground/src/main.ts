e2706e0af861c45f51bd41f8c00fdc949f11cc0d
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:51:44 +0900

    Update main.ts

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -7,9 +7,17 @@ import router from './router'
 const app = createApp(App)
 
 const hisonConfig: HisonConfig = getDefaultHisonConfig()
-hisonConfig.componentStyle.primaryColor = '#217346'
+//hisonConfig.componentStyle.primaryColor = '#217346'
 hisonConfig.componentStyle.size = Size.s
-hisonConfig.event.cssEvent.button_onBeforeClick = (e: MouseEvent) => {console.log('button_onBeforeClick');return true}
+
+const iconSpan = document.createElement('span')
+iconSpan.classList.add('material-symbols-rounded')
+iconSpan.textContent = 'auto_fix_high'
+hisonConfig.component.note.iconSpanElement.boldButtonIcon = iconSpan
+
+hisonConfig.event.cssEvent.button_onBeforeMouseover = () => {
+    return true;
+}
 
 app.use(hisonvue, hisonConfig)
 app.use(router)
