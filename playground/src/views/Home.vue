19097645b58e5696987db7a58512eddd14335ba1
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:51:22 +0900

    Update Home.vue

--- playground/src/views/Home.vue
+++ playground/src/views/Home.vue
@@ -1,16 +1,38 @@
 <template>
     <h1>Home Page</h1>
-    <HButton @click="goToGridTest" id="b1">Go to Grid Test Page</HButton><br><br>
-    <HButton @click="goToNoteTest" id="b2">Go to Note Test Page</HButton>
+    <HLayout class="hison-col-12-mb hison-col-12-tb hison-col-6-pc hison-col-6-wd">
+        <HButton
+        @click="goToNoteTest"
+        id="b1"
+        class="hison-col-2 hison-pos-left hison-size-m-mb hison-size-s-pc hison-color-danger-mb hison-color-warning-pc">
+            Go to Note<br>(left-button)
+        </HButton>
+        <div class="hison-col-5 hison-pos-center" style="text-align: center;">
+            <HButton @click="onClickCenterButton1" id="b3" class="hison-col-6 hison-size-l-mb hison-size-s-pc">center<br>button1</HButton>
+            <HButton @click="onClickCenterButton2" id="b4" class="hison-col-6 hison-size-l-mb hison-size-s-pc">center<br>button2</HButton>
+        </div>
+        <HButton @click="goToGridTest" id="b2" class="hison-col-2 hison-pos-right hison-size-l-mb hison-size-s-pc">Go to Grid<br>(right-button)</HButton>
+    </HLayout>
 </template>
 
 <script setup lang="ts">
+import { hison } from 'hisonvue'
 import { useRouter } from 'vue-router'
+
+let visibleToggle = false
+
 const router = useRouter()
 const goToGridTest = () => {
     router.push('/gridTest')
 }
 const goToNoteTest = () => {
     router.push('/noteTest')
+}
+const onClickCenterButton1 = () => {
+    hison.vue.getButton('b4')?.setDisable(visibleToggle)
+    visibleToggle = !visibleToggle
+}
+const onClickCenterButton2 = () => {
+    alert('test')
 }
 </script>
