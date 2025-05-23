9339c65baafd97272c4728c45a3a8955b9e2e057
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:57:20 +0900

    Update vanillanote.ts

--- src/plugins/vanillanote.ts
+++ src/plugins/vanillanote.ts
@@ -1,21 +1,19 @@
-import { getVanillanote, getVanillanoteConfig, type VanillanoteConfig } from 'vanillanote2'
-import { isValidHexColor, normalizeToHex } from '../utils';
-import { hisonCloser } from '..';
+import { getVanillanote } from 'vanillanote2'
+import { isValidHexColor, normalizeToHex } from '../utils'
+import { hisonCloser, HisonConfig } from '..'
 
-export const getVn = () => {
-    const vnConfig = getVanillanoteConfig();
-    setVnConfigWithHisonConfig(vnConfig);
-    const vn = getVanillanote(vnConfig);
-
-    return vn;
+export const getVn = (hisonConfig: HisonConfig) => {
+    setVnConfigWithHisonConfig(hisonConfig)
+    const vn = getVanillanote(hisonConfig.component.note)
+    return vn
 }
 
-const setVnConfigWithHisonConfig = (vnConfig: VanillanoteConfig) => {
+const setVnConfigWithHisonConfig = (hisonConfig: HisonConfig) => {
     if(hisonCloser.componentStyle.primaryColor) {
         const primaryColor = normalizeToHex(hisonCloser.componentStyle.primaryColor)
         if (!isValidHexColor(primaryColor)) {
           throw new Error(`[Hisonvue] Invalid mainColor: '${hisonCloser.componentStyle.primaryColor}'. Must be a valid hex color (e.g., '#ffffff').`)
         }
-        vnConfig.attributes.mainColor = primaryColor;
+        hisonConfig.component.note.attributes.mainColor = primaryColor
     }
 }
