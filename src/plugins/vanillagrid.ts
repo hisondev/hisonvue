e45d4b3a470952df608399c594c54c622b98ca17
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:57:13 +0900

    Update vanillagrid.ts

--- src/plugins/vanillagrid.ts
+++ src/plugins/vanillagrid.ts
@@ -1,37 +1,36 @@
-import { getVanillagrid, getVanillagridConfig, type VanillagridConfig } from 'vanillagrid2'
+import { getVanillagrid } from 'vanillagrid2'
 import { isValidHexColor, normalizeToHex } from '../utils';
 import { Size } from '../enums';
-import { hisonCloser } from '..';
+import { hisonCloser, HisonConfig } from '..';
 
-export const getVg = () => {
-    const vgConfig = getVanillagridConfig();
-    setVnConfigWithHisonConfig(vgConfig);
-    const vg = getVanillagrid(vgConfig);
+export const getVg = (hisonConfig: HisonConfig) => {
+    setVgConfigWithHisonConfig(hisonConfig);
+    const vg = getVanillagrid(hisonConfig.component.grid);
 
     return vg;
 }
 
-const setVnConfigWithHisonConfig = (vgConfig: VanillagridConfig) => {
+const setVgConfigWithHisonConfig = (hisonConfig: HisonConfig) => {
     if(hisonCloser.componentStyle.primaryColor) {
         const primaryColor = normalizeToHex(hisonCloser.componentStyle.primaryColor)
         if (!isValidHexColor(primaryColor)) {
           throw new Error(`[Hisonvue] Invalid mainColor: '${hisonCloser.componentStyle.primaryColor}'. Must be a valid hex color (e.g., '#ffffff').`)
         }
-        vgConfig.attributes.defaultGridCssInfo.color = primaryColor;
+        hisonConfig.component.grid.attributes.defaultGridCssInfo.color = primaryColor;
     }
     if(hisonCloser.componentStyle.size) {
         switch (hisonCloser.componentStyle.size) {
             case Size.s :
-                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 3;
+                hisonConfig.component.grid.attributes.defaultGridCssInfo.sizeLevel = 3;
                 break;
             case Size.m :
-                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 5;
+                hisonConfig.component.grid.attributes.defaultGridCssInfo.sizeLevel = 5;
                 break;
             case Size.l :
-                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 7;
+                hisonConfig.component.grid.attributes.defaultGridCssInfo.sizeLevel = 7;
                 break;
             case Size.xl :
-                vgConfig.attributes.defaultGridCssInfo.sizeLevel = 9;
+                hisonConfig.component.grid.attributes.defaultGridCssInfo.sizeLevel = 9;
                 break;
         }
     }
