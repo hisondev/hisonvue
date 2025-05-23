de6be0c89dc2a4a1c7808679500f45db91f6800f
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:57:48 +0900

    Update component.ts

--- src/types/component.ts
+++ src/types/component.ts
@@ -1,4 +1,6 @@
+import { GridMethods } from "vanillagrid2"
 import { GridAlign, GridVerticalAlign } from "../enums"
+import { VanillanoteElement } from "vanillanote2"
 
 export interface HGridColumn {
     id: string
@@ -39,9 +41,44 @@ export interface HGridColumn {
     fontUnderline?: boolean
 }
 
+export interface HGridMethods extends GridMethods{
+    getId(): string
+}
+
+export interface HNoteElement extends VanillanoteElement {
+    getId(): string
+}
+
 export interface HButtonMethods {
+    getId(): string
     isDisable(): boolean
     isVisible(): boolean
     setDisable(disable: boolean): void
     setVisible(visible: boolean): void
 }
+
+export interface HLayoutMethods {
+    getId(): string
+    isVisible(): boolean
+    setVisible(visible: boolean): void
+    getBackImageSrc(): string
+    setBackImageSrc(val: string): void
+    getBackImageRepeat(): string
+    setBackImageRepeat(val: string): void
+    getBackImageWidth(): string
+    setBackImageWidth(val: string): void
+    getBackImageAlign(): string
+    setBackImageAlign(val: string): void
+    getBackImageVerticalAlign(): string
+    setBackImageVerticalAlign(val: string): void
+    getBackColor(): string
+    setBackColor(val: string): void
+    getBorderColor(): string
+    setBorderColor(val: string): void
+    getBorderWidth(): string
+    setBorderWidth(val: string): void
+    getHeight(): string
+    setHeight(val: string): void
+}
+
+export type DeviceType = 'mb' | 'tb' | 'pc' | 'wd'
