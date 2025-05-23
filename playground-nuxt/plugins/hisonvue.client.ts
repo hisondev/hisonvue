df5eb16d726ba498aa00c6a93d599d4b42ac0a41
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:50:30 +0900

    Update hisonvue.client.ts

--- playground-nuxt/plugins/hisonvue.client.ts
+++ playground-nuxt/plugins/hisonvue.client.ts
@@ -2,11 +2,16 @@ import { defineNuxtPlugin } from '#app'
 import { getDefaultHisonConfig, hisonvue, Size, type HisonConfig } from 'hisonvue'
 import 'hisonvue/style.css'
 
-const hisonConfig: HisonConfig = getDefaultHisonConfig()
-hisonConfig.componentStyle.primaryColor = '#123456'
-hisonConfig.componentStyle.size = Size.s
-hisonConfig.componentStyle.invertColor = true
-
 export default defineNuxtPlugin((nuxtApp) => {
+  const hisonConfig: HisonConfig = getDefaultHisonConfig()
+  hisonConfig.componentStyle.primaryColor = '#123456'
+  hisonConfig.componentStyle.size = Size.s
+  hisonConfig.componentStyle.invertColor = true
+  
+  const iconSpan = document.createElement('span')
+  iconSpan.classList.add('material-symbols-rounded')
+  iconSpan.textContent = 'auto_fix_high'
+  hisonConfig.component.note.iconSpanElement.boldButtonIcon = iconSpan
+
   nuxtApp.vueApp.use(hisonvue, hisonConfig)
 })
