84cf595eaddb4bd26b2f65d86692c71eb7ab8284
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-23 16:55:47 +0900

    Update getDefaultHisonConfig.ts

--- src/core/getDefaultHisonConfig.ts
+++ src/core/getDefaultHisonConfig.ts
@@ -1,8 +1,11 @@
 import type { HisonConfig } from "../types";
 import type { InterfaceDataWrapper } from "hisonjs";
 import { DateFormat, DateTimeFormat, DayFormat, DayOfWeekFormat, HourFormat, HourMinuteFormat, MinuteFormat, MonthFormat, SecondFormat, Size, TimeFormat, YearFormat, YearMonthFormat } from "../enums";
+import { getVanillanoteConfig } from "vanillanote2";
+import { getVanillagridConfig } from "vanillagrid2";
+import { hison } from "..";
 
-export const primaryColorRGBA = 'rgba(96,169,232,1)'
+export const primaryColorRGBA = 'rgba(208,232,242,1)'
 export const mutedColorRGBA = 'rgba(158,158,158,1)'
 export const infoColorRGBA = 'rgba(121,232,252,1)'
 export const successColorRGBA = 'rgba(122,214,122,1)'
@@ -13,6 +16,79 @@ export const lightTextColorRGBA = 'rgba(255,255,255,1)'
 export const emptyColorRGBA = 'rgba(255,255,255,1)'
 
 export const getDefaultHisonConfig = () => {
+  const getIcon = (key: string) => {
+    const iconSpan = document.createElement('span');
+    iconSpan.classList.add('material-symbols-rounded');
+    iconSpan.textContent = key;
+    return iconSpan
+  }
+
+  //set note config
+  const vanillanoteConfig = getVanillanoteConfig();
+
+  //set grid config
+  const vanillagridConfig = getVanillagridConfig();
+  vanillagridConfig.footerFormula = {
+      "$$COUNT_CHECK" : function (colValues) {
+          let count = 0;
+          colValues.forEach((val) => {
+              if(val === 'Y') count = count + 1;
+          });
+          return String(count);
+      }
+  }
+  vanillagridConfig.dataType = {
+    radio : {
+        //cell의 style에 justify-content, text-align 속성을 지정한다.
+        cellStyle: {
+            justifyContent: "center",
+            textAlign: "center",
+        },
+        //cell이 선택된 상태에서 'Enter' key가 눌리면 해당 cell의 value를 "Y"로 변경한다.
+        onSelectedAndKeyDown : function (event, gridId, data) {
+            if(event.key === 'Enter' || event.key === ' ') {
+                hison.vue.getGrid(gridId)!.setColSameValue(data.colId, "N", true);
+                hison.vue.getGrid(gridId)!.setCellValue(data.rowIndex, data.colId, "Y", true);
+                event.stopPropagation();
+                event.preventDefault();
+                return false;
+            }
+            return false;
+        },
+        //cell을 마우스로 선택하면 cell의 value를 "Y"로 변경한다.
+        onClick : function (event, gridId, data) {
+            hison.vue.getGrid(gridId)!.setColSameValue(data.colId, "N", true);
+            hison.vue.getGrid(gridId)!.setCellValue(data.rowIndex, data.colId, "Y", true);
+        },
+        //value를 그대로 반환한다.
+        getValue: function (gridId, value) {
+            return value;
+        },
+        //value가 "Y"일 때 "true"를 아니면 "false"를 반환한다.
+        getText: function (gridId, value) {
+            const text = value === "Y" ? "true" : "false";
+            return text;
+        },
+        //radio type의 input이며 data가 "Y"일 때 checked 상태인 html 요소를 반환한다.
+        getChildNode: function (gridId, data) {
+            const childNode: any = document.createElement("input");
+            childNode.setAttribute("type", "radio");
+            childNode.setAttribute("name", data.name? data.name : data.colId);
+            childNode.setAttribute("value", "" + data.rowIndex);
+            childNode._gridId = gridId;
+            childNode.rowIndex = data.rowIndex;
+            childNode.colId = data.colId;
+            childNode.checked = data.value === "Y";
+            return childNode;
+        },
+        //filter는 체크된 값은 "●", 아닌 값은 "○"를 적용한다.
+        getFilterValue: function (gridId, value) {
+            const filterValue = value === "Y" ? "●" : "○";
+            return filterValue;
+        },
+    }
+  }
+
   const hisonConfig: HisonConfig = {
     //utils
     dateFormat : DateFormat['yyyy-MM-dd'],
@@ -70,7 +146,10 @@ export const getDefaultHisonConfig = () => {
     afterDeleteUrlRequest(responseData: { data: any; response: Response }): boolean | void { return true; },
     interceptApiResult(result: InterfaceDataWrapper | undefined, response: Response): boolean | void { return true; },
     interceptApiError(error: any): boolean | void { return true; },
-    
+    component : {
+      note : vanillanoteConfig,
+      grid : vanillagridConfig,
+    },
     componentStyle : {
       size: Size.m,
       filledColor: primaryColorRGBA,
