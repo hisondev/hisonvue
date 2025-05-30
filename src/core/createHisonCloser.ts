f51055bfd16037450f598c4d21c8dd7181b316af
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-30 16:32:20 +0900

    Update createHisonCloser.ts

--- src/core/createHisonCloser.ts
+++ src/core/createHisonCloser.ts
@@ -4,6 +4,10 @@ import { getVg, getVn } from "../plugins"
 
 export const createHisonCloser = (hisonConfig: HisonConfig) => {
   // 이벤트 설정
+  if(hisonConfig.event.cssEvent.button_onBeforeFocus) hisonCloser.event.cssEvent.button_onBeforeFocus = hisonConfig.event.cssEvent.button_onBeforeFocus
+  if(hisonConfig.event.cssEvent.button_onAfterFocus) hisonCloser.event.cssEvent.button_onAfterFocus = hisonConfig.event.cssEvent.button_onAfterFocus
+  if(hisonConfig.event.cssEvent.button_onBeforeBlur) hisonCloser.event.cssEvent.button_onBeforeBlur = hisonConfig.event.cssEvent.button_onBeforeBlur
+  if(hisonConfig.event.cssEvent.button_onAfterBlur) hisonCloser.event.cssEvent.button_onAfterBlur = hisonConfig.event.cssEvent.button_onAfterBlur
   if(hisonConfig.event.cssEvent.button_onBeforeClick) hisonCloser.event.cssEvent.button_onBeforeClick = hisonConfig.event.cssEvent.button_onBeforeClick
   if(hisonConfig.event.cssEvent.button_onAfterClick) hisonCloser.event.cssEvent.button_onAfterClick = hisonConfig.event.cssEvent.button_onAfterClick
   if(hisonConfig.event.cssEvent.button_onBeforeMouseover) hisonCloser.event.cssEvent.button_onBeforeMouseover = hisonConfig.event.cssEvent.button_onBeforeMouseover
@@ -15,14 +19,18 @@ export const createHisonCloser = (hisonConfig: HisonConfig) => {
   if(hisonConfig.event.cssEvent.button_onBeforeTouchend) hisonCloser.event.cssEvent.button_onBeforeTouchend = hisonConfig.event.cssEvent.button_onBeforeTouchend
   if(hisonConfig.event.cssEvent.button_onAfterTouchend) hisonCloser.event.cssEvent.button_onAfterTouchend = hisonConfig.event.cssEvent.button_onAfterTouchend
   
-  if(hisonConfig.event.cssEvent.textbox_onBeforeMouseover) hisonCloser.event.cssEvent.textbox_onBeforeMouseover = hisonConfig.event.cssEvent.textbox_onBeforeMouseover
-  if(hisonConfig.event.cssEvent.textbox_onAfterMouseover) hisonCloser.event.cssEvent.textbox_onAfterMouseover = hisonConfig.event.cssEvent.textbox_onAfterMouseover
-  if(hisonConfig.event.cssEvent.textbox_onBeforeMouseout) hisonCloser.event.cssEvent.textbox_onBeforeMouseout = hisonConfig.event.cssEvent.textbox_onBeforeMouseout
-  if(hisonConfig.event.cssEvent.textbox_onAfterMouseout) hisonCloser.event.cssEvent.textbox_onAfterMouseout = hisonConfig.event.cssEvent.textbox_onAfterMouseout
-  if(hisonConfig.event.cssEvent.textbox_onBeforeTouchstart) hisonCloser.event.cssEvent.textbox_onBeforeTouchstart = hisonConfig.event.cssEvent.textbox_onBeforeTouchstart
-  if(hisonConfig.event.cssEvent.textbox_onAfterTouchstart) hisonCloser.event.cssEvent.textbox_onAfterTouchstart = hisonConfig.event.cssEvent.textbox_onAfterTouchstart
-  if(hisonConfig.event.cssEvent.textbox_onBeforeTouchend) hisonCloser.event.cssEvent.textbox_onBeforeTouchend = hisonConfig.event.cssEvent.textbox_onBeforeTouchend
-  if(hisonConfig.event.cssEvent.textbox_onAfterTouchend) hisonCloser.event.cssEvent.textbox_onAfterTouchend = hisonConfig.event.cssEvent.textbox_onAfterTouchend
+  if(hisonConfig.event.cssEvent.input_onBeforeFocus) hisonCloser.event.cssEvent.input_onBeforeFocus = hisonConfig.event.cssEvent.input_onBeforeFocus
+  if(hisonConfig.event.cssEvent.input_onAfterFocus) hisonCloser.event.cssEvent.input_onAfterFocus = hisonConfig.event.cssEvent.input_onAfterFocus
+  if(hisonConfig.event.cssEvent.input_onBeforeBlur) hisonCloser.event.cssEvent.input_onBeforeBlur = hisonConfig.event.cssEvent.input_onBeforeBlur
+  if(hisonConfig.event.cssEvent.input_onAfterBlur) hisonCloser.event.cssEvent.input_onAfterBlur = hisonConfig.event.cssEvent.input_onAfterBlur
+  if(hisonConfig.event.cssEvent.input_onBeforeMouseover) hisonCloser.event.cssEvent.input_onBeforeMouseover = hisonConfig.event.cssEvent.input_onBeforeMouseover
+  if(hisonConfig.event.cssEvent.input_onAfterMouseover) hisonCloser.event.cssEvent.input_onAfterMouseover = hisonConfig.event.cssEvent.input_onAfterMouseover
+  if(hisonConfig.event.cssEvent.input_onBeforeMouseout) hisonCloser.event.cssEvent.input_onBeforeMouseout = hisonConfig.event.cssEvent.input_onBeforeMouseout
+  if(hisonConfig.event.cssEvent.input_onAfterMouseout) hisonCloser.event.cssEvent.input_onAfterMouseout = hisonConfig.event.cssEvent.input_onAfterMouseout
+  if(hisonConfig.event.cssEvent.input_onBeforeTouchstart) hisonCloser.event.cssEvent.input_onBeforeTouchstart = hisonConfig.event.cssEvent.input_onBeforeTouchstart
+  if(hisonConfig.event.cssEvent.input_onAfterTouchstart) hisonCloser.event.cssEvent.input_onAfterTouchstart = hisonConfig.event.cssEvent.input_onAfterTouchstart
+  if(hisonConfig.event.cssEvent.input_onBeforeTouchend) hisonCloser.event.cssEvent.input_onBeforeTouchend = hisonConfig.event.cssEvent.input_onBeforeTouchend
+  if(hisonConfig.event.cssEvent.input_onAfterTouchend) hisonCloser.event.cssEvent.input_onAfterTouchend = hisonConfig.event.cssEvent.input_onAfterTouchend
 
   //컴포넌트 스타일 설정 
   hisonCloser.componentStyle = {
@@ -117,5 +125,6 @@ export const createHisonCloser = (hisonConfig: HisonConfig) => {
     buttonList: {},
     layoutList: {},
     inputList: {},
+    dataGroupList: {},
   }
 }
