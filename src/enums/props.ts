8d5d728c6311414b978a91ca6be6af38b06041e3
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-30 16:32:01 +0900

    Update props.ts

--- src/enums/props.ts
+++ src/enums/props.ts
@@ -1,4 +1,28 @@
 export enum BoolString {
     true = 'true',
-    false = 'false'
+    false = 'false',
+}
+
+export enum InputType {
+    text = 'text',
+    mask = 'mask',
+    number = 'number',
+    digit = 'digit',
+    date = 'date',
+    month = 'month',
+    email = 'email',
+    password = 'password',
+}
+
+export enum EditMode {
+    editable = 'editable',
+    disable = 'disable',
+    readonly = 'readonly',
+}
+
+export enum DataStatus {
+    R = 'R',
+    C = 'C',
+    U = 'U',
+    D = 'D',
 }
