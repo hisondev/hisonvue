90e437d59ac4384cbfa9501f922aa094cb8ad649
hisondev <64723261+hisondev@users.noreply.github.com>
2025-05-30 16:27:13 +0900

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,82 @@
-import type { DeviceType } from "../types";
+import type { DeviceType } from "../types"
+
+export const isNullOrUndefined = (value: any) => {
+  if(value === null || value === undefined) return true
+  return false
+}
 
 export const getUUID = () => {
-    let d = new Date().getTime();
+    let d = new Date().getTime()
     if (typeof performance !== "undefined" && typeof performance.now === "function"){
-        d += performance.now();
+        d += performance.now()
     }
     return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-        const r = (d + Math.random() * 16) % 16 | 0;
-        d = Math.floor(d / 16);
-        return (c === "x" ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+        const r = (d + Math.random() * 16) % 16 | 0
+        d = Math.floor(d / 16)
+        return (c === "x" ? r : (r & 0x3 | 0x8)).toString(16)
+    })
+}
+
+export const toLowerCase = (value: string | null) => {
+    if (!value || typeof value !== 'string') return '';
+    return value.toLowerCase();
 };
+export const toUpperCase = (value: string | null) => {
+    if (!value || typeof value !== 'string') return '';
+    return value.toUpperCase();
+};
+
+export const getMaskValue = (value: string, format: string) => {
+  if(typeof format !== 'string') return null;
+  if(typeof value !== 'string') return null;
+  const formatArr = format.split('');
+  const valueArr = value.split('');
+  let lastValue = '';
+  let isValid;
+  formatArr.every((formatPiece, idx) => {
+      isValid = false;
+      switch (formatPiece) {
+        case 'A':
+            if (/^[A-Z]$/.test(valueArr[idx])) {
+                lastValue += valueArr[idx];
+                isValid = true;
+            }
+            else if (/^[a-z]$/.test(valueArr[idx])) {
+                lastValue += toUpperCase(valueArr[idx]);
+                isValid = true;
+            }
+            break;
+        case 'a':
+            if (/^[a-z]$/.test(valueArr[idx])) {
+                lastValue += valueArr[idx];
+                isValid = true;
+            }
+            else if (/^[A-Z]$/.test(valueArr[idx])) {
+                lastValue += toLowerCase(valueArr[idx]);
+                isValid = true;
+            }
+            break;
+        case '9':
+            if (/^[0-9]$/.test(valueArr[idx])) {
+                lastValue += valueArr[idx];
+                isValid = true;
+            }
+            break;
+        default:
+            if (formatPiece === valueArr[idx]) {
+                lastValue += valueArr[idx];
+                isValid = true;
+            }
+            break;
+      }
+      return isValid;
+  })
+  return lastValue;
+};
+
+export const getDigitsOnly = (value: string) => {
+  return value.replace(/[^0-9]/g, '');
+}
 
 export const getDeviceType = (width: number): DeviceType => {
   if (width < 768) return 'mb'
@@ -19,11 +85,11 @@ export const getDeviceType = (width: number): DeviceType => {
   return 'wd'
 }
 
-export const getColorClass = (el: HTMLElement, suffix: string) => {
+export const getColorClass = (el: HTMLElement, elTypeName: string, suffix: string) => {
     const color = getSpecificClassNameFromClassList(el.classList, 'color')
-    if(color) return `hison-color-${color}-${suffix}`
+    if(color) return `hison-color-${color}-${elTypeName}-${suffix}`
 
-    return `hison-color-primary-${suffix}`
+    return `hison-color-primary-${elTypeName}-${suffix}`
 }
 
 export const getSpecificClassNameFromClassList = (classList: DOMTokenList | string[], classStarts: string) => {
