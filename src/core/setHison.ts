93778bdb14753ede9e9282456ee634727176e2bf
hisondev <64723261+hisondev@users.noreply.github.com>
2025-06-04 16:39:32 +0900

    Update setHison.ts

--- src/core/setHison.ts
+++ src/core/setHison.ts
@@ -85,5 +85,6 @@ export const setHison = (hison: Hison, hisonConfig: HisonConfig) => {
     hison.vue.getButton = (buttonId: string) => { return hisonCloser.component.buttonList[buttonId] }
     hison.vue.getLayout = (layoutId: string) => { return hisonCloser.component.layoutList[layoutId] }
     hison.vue.getInput = (inputId: string) => { return hisonCloser.component.inputList[inputId] }
-    hison.vue.getDataGroup = (dataGroupId: string) => { return hisonCloser.component.dataGroupList[dataGroupId] }
+    hison.vue.getInputGroup = (inputGroupId: string) => { return hisonCloser.component.inputGroupList[inputGroupId] }
+    hison.vue.getCalendar = (calendarId: string) => { return hisonCloser.component.calendarList[calendarId] }
 }
