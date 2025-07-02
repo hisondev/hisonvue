f870341ad36950713c1c9324ffe4a2aa8ef76da1
hisondev <64723261+hisondev@users.noreply.github.com>
2025-07-02 16:52:10 +0900

    Create chart.ts

---
+++ src/plugins/chart.ts
@@ -0,0 +1,52 @@
+import {
+  Chart,
+  ArcElement,
+  BarController,
+  BarElement,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  LineElement,
+  PieController,
+  PointElement,
+  RadarController,
+  RadialLinearScale,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+  Filler,
+} from 'chart.js'
+import { hisonCloser, HisonConfig } from '..'
+
+Chart.register(
+  // controllers
+  BarController,
+  LineController,
+  RadarController,
+  PieController,
+  DoughnutController,
+  BubbleController,
+  ScatterController,
+  // elements
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  // scales
+  CategoryScale,
+  LinearScale,
+  RadialLinearScale,
+  // plugins
+  Tooltip,
+  Legend,
+  Filler
+)
+
+export const configureChartDefaults = (hisonConfig: HisonConfig) => {
+    const userDefaults = hisonConfig.component.chart
+    if (userDefaults) {
+        Object.assign(Chart.defaults, userDefaults)
+    }
+}
