afc56b06aff2baa7a0278d06345eebbf051ad1d3
hisondev <64723261+hisondev@users.noreply.github.com>
2025-09-23 16:38:55 +0900

    Update HButton.vue

--- src/components/HButton/HButton.vue
+++ src/components/HButton/HButton.vue
@@ -1,37 +1,55 @@
 <template>
-  <button
-    ref="buttonRef"
+  <div
     :class="[
-      'hison-button',
+      'hison-wrapper',
       ...responsiveClassList,
-      visibleClass,
-      disableClass,
-      backgroundTypeClass
+      visibleClass
     ]"
-    :style="props.style"
-    :disabled="disable"
-    :title="title || undefined"
-    @click="onClick"
-    @mousedown="$emit('mousedown', $event, buttonMethods)"
-    @mouseup="$emit('mouseup', $event, buttonMethods)"
-    @mouseover="$emit('mouseover', $event, buttonMethods)"
-    @mouseout="$emit('mouseout', $event, buttonMethods)"
   >
-    <template v-if="$slots.icon">
-      <slot name="icon" />
-    </template>
-    <slot v-if="hasSlot" />
-    <span v-else v-html="internalTextHtml"></span>
-  </button>
+    <button
+      ref="buttonRef"
+      :class="[
+        'hison-button',
+        disableClass,
+        borderClass,
+        backgroundTypeClass
+      ]"
+      :style="props.style"
+      :disabled="disable"
+      :title="title || undefined"
+      :tabindex="tabIndex ?? undefined"
+      @click.stop="onClick"
+      @mousedown.stop="$emit('mousedown', $event, buttonMethods)"
+      @mouseup.stop="$emit('mouseup', $event, buttonMethods)"
+      @mouseover.stop="$emit('mouseover', $event, buttonMethods)"
+      @mouseout.stop="$emit('mouseout', $event, buttonMethods)"
+      :aria-label="computedAriaLabel"
+    >
+      <template v-if="$slots.icon">
+        <slot name="icon" />
+      </template>
+      <template v-if="hasLabelSlot">
+        <slot name="label" v-if="$slots.label" />
+        <slot v-else />
+      </template>
+      <span v-else class="hison-button-text">{{ internalText }}</span>
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, ref, onMounted, onBeforeUnmount, nextTick, watch, useSlots, unref } from 'vue'
+import {
+  defineComponent, computed, ref, onMounted, onBeforeUnmount, nextTick, watch,
+  useSlots, unref, Text, Comment, type VNode
+} from 'vue'
 import type { HButtonMethods } from '../../types'
 import { addButtonCssEvent, removeButtonCssEvent } from '../common/setButtonCssEvent'
 import { buttonProps } from './props'
 import { BackgroundType, hisonCloser } from '../..'
-import { addComponentNameToClass, extractResponsiveClasses, getSpecificClassValueFromClassList, getUUID, registerReloadable, reloadHisonComponent, unregisterReloadable } from '../../utils'
+import {
+  extractResponsiveClasses, getUUID, addComponentNameToClass,
+  registerReloadable, reloadHisonComponent, toClassString, unregisterReloadable
+} from '../../utils'
 import { useDevice } from '../../core'
 
 export default defineComponent({
@@ -47,38 +65,74 @@ export default defineComponent({
     const device = useDevice()
     const slots = useSlots()
     const isPending = ref(false)
-    const clickInterval = ref(props.clickInterval || 0) // ref로 내부에서만 관리
+    const clickInterval = ref(props.clickInterval || 0)
     const lastClickTime = ref(0)
 
     const visible = ref(props.visible)
     const disable = ref(props.disable)
     const title = ref(props.title || '')
 
-    const visibleClass = computed(() => visible.value ? '' : 'hison-display-none')
-    const disableClass = computed(() => disable.value ? 'hison-disable' : '')
+    const visibleClass = computed(() => (visible.value ? '' : 'hison-display-none'))
+    const disableClass = computed(() => (disable.value ? 'hison-disable' : ''))
+    const border = ref<boolean>(props.border ?? false)
+    const borderClass = computed(() => (border.value ? 'hison-border' : ''))
     const backgroundType = ref(props.backgroundType || BackgroundType.filled)
     const backgroundTypeClass = computed(() => {
       switch (backgroundType.value) {
-        case BackgroundType.empty:
-          return 'hison-bg-empty'
-        case BackgroundType.transparent:
-          return 'hison-bg-transparent'
-        default:
-          return ''
+        case BackgroundType.empty: return 'hison-bg-empty'
+        case BackgroundType.transparent: return 'hison-bg-transparent'
+        default: return 'hison-bg-filled'
       }
     })
 
-    const hasSlot = computed(() => !!slots.default)
     const internalText = ref(props.text || '')
-    const internalTextHtml = computed(() =>
-        hasSlot.value ? '' : (internalText.value || '').replace(/\n/g, '<br>')
+    const absorbedDefaultAsText = ref(false)
+    const tabIndex = ref<number | null>(
+      props.tabIndex !== null && props.tabIndex !== '' ? Number(props.tabIndex) : null
     )
 
+    const hasNonTrivial = (nodes?: VNode[]) => {
+      if (!nodes || !nodes.length) return false
+      return nodes.some(n => {
+        if (n.type === Comment) return false
+        if (n.type === Text) return !/^\s*$/.test(String(n.children ?? ''))
+        return true
+      })
+    }
+
+    const initialDefaultNodes = slots.default?.() ?? []
+    const isTextOnlyDefaultInitial =
+      initialDefaultNodes.length > 0 && initialDefaultNodes.every(n => n.type === Text)
+
+    if (!slots.label && isTextOnlyDefaultInitial) {
+      const merged = initialDefaultNodes
+        .map(n => String((n as any).children ?? ''))
+        .join('')
+        .trim()
+      internalText.value = merged
+      absorbedDefaultAsText.value = true
+    }
+
+    const defaultNodes = computed(() => slots.default?.() ?? [])
+    const hasDefaultSlotContent = computed(() => hasNonTrivial(defaultNodes.value))
+
+    const hasLabelSlot = computed(() => {
+      if (slots.label) return true
+      if (absorbedDefaultAsText.value) return false
+      return hasDefaultSlotContent.value
+    })
+
+    const computedAriaLabel = computed(() => {
+      const hasIconOnly = !!slots.icon && !hasLabelSlot.value && !internalText.value
+      if (hasIconOnly) return props.title || 'button'
+      return undefined
+    })
+
     const responsiveClassList = ref<string[]>([])
-    const refleshResponsiveClassList = () => {
-      responsiveClassList.value = extractResponsiveClasses(props.class || '', device.value)
-      addComponentNameToClass(responsiveClassList.value, 'size', 'button', hisonCloser.componentStyle.size)
-      addComponentNameToClass(responsiveClassList.value, 'color', 'button', 'primary')
+    const refreshResponsiveClassList = () => {
+      responsiveClassList.value = extractResponsiveClasses(toClassString(props.class) || '', device.value)
+      addComponentNameToClass(responsiveClassList.value, 'size', hisonCloser.componentStyle.size)
+      addComponentNameToClass(responsiveClassList.value, 'color', 'primary')
     }
 
     const onClick = async (e: MouseEvent) => {
@@ -92,7 +146,7 @@ export default defineComponent({
         const resultArray = Array.isArray(results) ? results : []
         await Promise.all(
           resultArray
-            .filter(res => res && typeof res.then === 'function')
+            .filter(res => res && typeof (res as any).then === 'function')
             .map(res => res as Promise<any>)
         )
       } finally {
@@ -106,32 +160,38 @@ export default defineComponent({
         unmount()
         nextTick(mount)
       })
-      if (!buttonRef.value) return
-      
-      refleshResponsiveClassList()
-      addButtonCssEvent(buttonRef.value)
+      if (buttonRef.value) addButtonCssEvent(buttonRef.value)
+      refreshResponsiveClassList()
+
       buttonMethods.value = {
         getId: () => id,
         getType : () => 'button',
-        getText: () => hasSlot.value ? '' : internalText.value,
+        getText: () => (hasLabelSlot.value ? '' : internalText.value),
         getTitle: () => title.value,
         isVisible: () => visible.value,
         isDisable: () => disable.value,
-        setText: (val: string) => { if (!hasSlot.value) internalText.value = val },
+        setText: (val: string) => { if (!hasLabelSlot.value) internalText.value = val },
         setTitle: (val: string) => { title.value = val },
         setVisible: (val: boolean) => { visible.value = val },
         setDisable: (val: boolean) => { disable.value = val },
+        isBorder: () => border.value,
+        setBorder: (val: boolean) => { border.value = val },
         getBackgroundType: () => backgroundType.value,
         setBackgroundType: (type: BackgroundType) => { backgroundType.value = type },
         unlock: () => { isPending.value = false },
         getClickInterval: () => clickInterval.value,
         setClickInterval: (ms: number) => {
           clickInterval.value = ms
-          lastClickTime.value = 0 // 즉시 클릭 가능하게 초기화 (선택)
+          lastClickTime.value = 0
         },
-        focus: () => { buttonRef.value?.focus(); },
-        reload: () => reloadHisonComponent(reloadId)
+        getTabIndex: () => tabIndex.value,
+        setTabIndex: (v: number | null) => {
+          tabIndex.value = v !== null && v !== undefined ? Number(v) : null
+        },
+        focus: () => { buttonRef.value?.focus() },
+        reload: () => reloadHisonComponent(reloadId),
       }
+
       hisonCloser.component.buttonList[id] = buttonMethods.value
       emit('mounted', buttonMethods.value)
     }
@@ -139,33 +199,33 @@ export default defineComponent({
     const unmount = () => {
       unregisterReloadable(reloadId)
       delete hisonCloser.component.buttonList[id]
-      if (buttonRef.value) {
-        removeButtonCssEvent(buttonRef.value)
-      }
+      if (buttonRef.value) removeButtonCssEvent(buttonRef.value)
     }
 
     onMounted(mount)
     onBeforeUnmount(unmount)
 
-    watch(device, (newDevice) => {
-      refleshResponsiveClassList()
-      emit('responsive-change', newDevice)
+    watch(device, () => {
+      refreshResponsiveClassList()
+      emit('responsive-change', device.value)
     })
 
     return {
-        buttonRef,
-        buttonMethods: computed(() => unref(buttonMethods)),
-        props,
-        visibleClass,
-        disable,
-        disableClass,
-        backgroundTypeClass,
-        title,
-        responsiveClassList,
-        internalText,
-        internalTextHtml,
-        hasSlot,
-        onClick,
+      buttonRef,
+      buttonMethods: computed(() => unref(buttonMethods)),
+      props,
+      visibleClass,
+      disable,
+      disableClass,
+      borderClass,
+      backgroundTypeClass,
+      title,
+      responsiveClassList,
+      hasLabelSlot,
+      computedAriaLabel,
+      internalText,
+      tabIndex,
+      onClick,
     }
   }
 })
