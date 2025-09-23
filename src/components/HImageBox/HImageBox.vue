558313dd7f0123e55bb72741569d2e93d9f28358
hisondev <64723261+hisondev@users.noreply.github.com>
2025-09-23 16:43:22 +0900

    Update HImageBox.vue

--- src/components/HImageBox/HImageBox.vue
+++ src/components/HImageBox/HImageBox.vue
@@ -4,6 +4,7 @@
       'hison-imagebox',
       ...responsiveClassList,
       visibleClass,
+      borderClass,
       editModeClass,
     ]"
     :style="props.style"
@@ -33,47 +34,50 @@
       <div class="imgbox-btn-row" v-if="!readonly">
         <HButton
           v-if="!imageValue"
-          :id="`hison_input_imagebox_add_button_${id}`"
+          :id="`hison_imagebox_add_button_${id}`"
           :disable="disable"
           :class="['hison-col-12', ...buttonClassList]"
+          :tabindex="tabIndex ?? undefined"
           @click="openFileDialog"
-          @focus="$emit('focus', imageBoxMethods)"
-          @blur="$emit('blur', imageBoxMethods)"
+          @focus="$emit('focus', imageboxMethods)"
+          @blur="$emit('blur', imageboxMethods)"
         >
           <template v-if="$slots['add-button']">
             <slot name="add-button" :add="openFileDialog" />
           </template>
           <template v-else>
-            <span v-html="addButtonTextHtml"></span>
+            <span class="hison-imagebox-add-text">{{ addButtonText }}</span>
           </template>
         </HButton>
+
         <template v-else>
           <HButton
-            :id="`hison_input_imagebox_add_button_${id}`"
+            :id="`hison_imagebox_add_button_${id}`"
             :disable="disable"
-            :class="['hison-col-6',...buttonClassList]"
+            :class="['hison-col-6', ...buttonClassList]"
             @click="openFileDialog"
-            @focus="$emit('focus', imageBoxMethods)"
-            @blur="$emit('blur', imageBoxMethods)"
+            @focus="$emit('focus', imageboxMethods)"
+            @blur="$emit('blur', imageboxMethods)"
           >
             <template v-if="$slots['add-button']">
               <slot name="add-button" :add="openFileDialog" />
             </template>
             <template v-else>
-              <span v-html="addButtonTextHtml"></span>
+              <span class="hison-imagebox-add-text">{{ addButtonText }}</span>
             </template>
           </HButton>
+
           <HButton
-            :id="`hison_input_imagebox_remove_button_${id}`"
+            :id="`hison_imagebox_remove_button_${id}`"
             :disable="disable"
-            :class="['hison-col-6',...buttonClassList]"
+            :class="['hison-col-6', ...buttonClassList]"
             @click="removeImage"
           >
             <template v-if="$slots['remove-button']">
               <slot name="remove-button" :remove="removeImage" />
             </template>
             <template v-else>
-              <span v-html="removeButtonTextHtml"></span>
+              <span class="hison-imagebox-remove-text">{{ removeButtonText }}</span>
             </template>
           </HButton>
         </template>
@@ -94,16 +98,16 @@
 
 <script lang="ts">
 import { defineComponent, ref, computed, nextTick, onMounted, onBeforeUnmount, watch } from 'vue'
-import { imageBoxProps } from './props'
+import { imageboxProps } from './props'
 import { EditMode } from '../../enums'
-import { AttachedFileItem, HImageBoxMethods, hison, hisonCloser } from '../..'
-import { getUUID, extractResponsiveClasses, addComponentNameToClass, registerReloadable, extractPrefixedClasses, getIndexSpecificClassNameFromClassList, reloadHisonComponent } from '../../utils'
+import { AttachedFileItem, HImageboxMethods, hison, hisonCloser } from '../..'
+import { getUUID, extractResponsiveClasses, registerReloadable, extractPrefixedClasses, getIndexSpecificClassNameFromClassList, reloadHisonComponent, toClassString, addComponentNameToClass } from '../../utils'
 import { useDevice } from '../../core'
 import { InterfaceDataModel } from 'hisonjs'
 
 export default defineComponent({
-  name: 'HImageBox',
-  props: imageBoxProps,
+  name: 'HImagebox',
+  props: imageboxProps,
   emits: [
     'mounted',
     'responsive-change',
@@ -118,14 +122,16 @@ export default defineComponent({
     const fileInputRef = ref<HTMLInputElement | null>(null)
     const id = props.id ? props.id : getUUID()
     const reloadId = `himagebox:${id}`
-    const imageBoxMethods = ref<HImageBoxMethods | null>(null)
+    const imageboxMethods = ref<HImageboxMethods | null>(null)
 
     const visible = ref(props.visible)
     const editMode = ref(props.editMode)
     const disable = computed(() => editMode.value === EditMode.disable)
     const readonly = computed(() => editMode.value === EditMode.readonly)
+    const border = ref<boolean>(props.border ?? false)
     const editModeClass = computed(() => editMode.value !== EditMode.editable ? `hison-imagebox-${editMode.value}` : '')
     const visibleClass = computed(() => visible.value ? '' : 'hison-display-none')
+    const borderClass = computed(() => (border.value ? 'hison-border' : ''))
     
     const attId = ref(props.attId || '')
 
@@ -134,47 +140,36 @@ export default defineComponent({
 
     const placeholder = ref(props.placeholder)
     const addButtonText = ref(props.addButtonText)
-    const addButtonTextHtml = computed(() => (addButtonText.value || '').replace(/\n/g, '<br>'))
     const removeButtonText = ref(props.removeButtonText)
-    const removeButtonTextHtml = computed(() => (removeButtonText.value || '').replace(/\n/g, '<br>'))
     const allowedTypes = ref<string[]>(Array.isArray(props.allowedTypes) ? props.allowedTypes : props.allowedTypes ? props.allowedTypes.split(',') : [])
     const disallowedTypes = ref<string[]>(Array.isArray(props.disallowedTypes) ? props.disallowedTypes : props.disallowedTypes ? props.disallowedTypes.split(',') : [])
-    const maxFileSize = ref<number>(props.maxFileSize ?? hison.getMaxFileSetSize())
+    const maxFileSize = ref<number>(props.maxFileSize ?? hison.getMaxFilesetSize())
     const onDisallowedType = ref(typeof props.onDisallowedType === 'function' ? props.onDisallowedType : undefined)
     const onMaxFileSizeExceeded = ref(typeof props.onMaxFileSizeExceeded === 'function' ? props.onMaxFileSizeExceeded : undefined)
 
     const imageValue = ref<AttachedFileItem | null>(props.modelValue)
     const isModified = ref(false)
 
-    // accept string for file input
     const accept = computed(() => {
       if (allowedTypes.value.length === 0) return ''
-
       const disallowedSet = new Set(disallowedTypes.value.map(t => t.toLowerCase()))
       const filtered = allowedTypes.value.filter(t => !disallowedSet.has(t.toLowerCase()))
-
       return filtered.length > 0 ? filtered.join(',') : undefined
     })
 
-    // previewUrl: 파일은 FileReader, string은 바로 사용
     const previewUrl = ref<string | null>(null)
     const setPreviewUrl = (val: AttachedFileItem | null) => {
       if (!val || val.isDeleted) {
         previewUrl.value = null
         return
       }
-      // DB 파일: filePath(또는 서버에서 내려준 URL)
       if (val.filePath) {
         previewUrl.value = val.filePath
-      }
-      // 새로 업로드된 파일: File 객체 → base64 변환
-      else if (val.file instanceof File) {
+      } else if (val.file instanceof File) {
         const reader = new FileReader()
         reader.onload = e => previewUrl.value = String(e.target?.result || '')
         reader.readAsDataURL(val.file)
-      }
-      // 혹시 fileName에 base64 문자열을 직접 넣는 경우 등
-      else if (val.fileName && val.fileName.startsWith('data:')) {
+      } else if (val.fileName && val.fileName.startsWith('data:')) {
         previewUrl.value = val.fileName
       } else {
         previewUrl.value = null
@@ -182,27 +177,28 @@ export default defineComponent({
     }
     setPreviewUrl(imageValue.value)
 
-    // responsive class
+    const tabIndex = ref<number | null>(
+      props.tabIndex !== null && props.tabIndex !== '' ? Number(props.tabIndex) : null
+    )
+
     const device = useDevice()
     const responsiveClassList = ref<string[]>([])
-    const refleshResponsiveClassList = () => {
-      responsiveClassList.value = extractResponsiveClasses(props.class || '', device.value)
+    const refreshResponsiveClassList = () => {
+      responsiveClassList.value = extractResponsiveClasses(toClassString(props.class) || '', device.value)
       if (getIndexSpecificClassNameFromClassList(responsiveClassList.value, 'col') === -1) responsiveClassList.value.push('hison-col-12')
-      addComponentNameToClass(responsiveClassList.value, 'size', 'imagebox', hisonCloser.componentStyle.size)
-      addComponentNameToClass(responsiveClassList.value, 'color', 'imagebox', 'primary')
+      addComponentNameToClass(responsiveClassList.value, 'size', hisonCloser.componentStyle.size)
+      addComponentNameToClass(responsiveClassList.value, 'color', 'primary')
     }
     const buttonClassList = computed(() => {
       const classList = []
-      classList.push(...extractPrefixedClasses(props.class || '', 'size'))
-      classList.push(...extractPrefixedClasses(props.class || '', 'color'))
+      classList.push(...extractPrefixedClasses(toClassString(props.class) || '', 'size'))
+      classList.push(...extractPrefixedClasses(toClassString(props.class) || '', 'color'))
       return classList
     })
 
-    // 버튼 show/hide 상태
     const showAddButton = computed(() => editMode.value !== EditMode.readonly)
     const showRemoveButton = computed(() => !!imageValue.value && editMode.value !== EditMode.readonly)
 
-    // 파일 type/size 체크
     const isFileTypeAllowed = (file: File): boolean => {
       const type = file.type
       const ext = '.' + file.name.split('.').pop()?.toLowerCase()
@@ -257,11 +253,10 @@ export default defineComponent({
       setPreviewUrl(newItem)
       isModified.value = true
       emit('update:modelValue', newItem)
-      emit('add', newItem, imageBoxMethods.value)
-      emit('change', newItem, imageBoxMethods.value)
+      emit('add', newItem, imageboxMethods.value)
+      emit('change', newItem, imageboxMethods.value)
     }
 
-    // 이미지 업로드/변경
     const onFileChange = (e: Event) => {
       if (editMode.value !== EditMode.editable) return
       const files = (e.target as HTMLInputElement).files
@@ -270,7 +265,6 @@ export default defineComponent({
       nextTick(() => { if (fileInputRef.value) fileInputRef.value.value = '' })
     }
     
-    // 드래그 핸들러
     const handleDragEnter = (e: DragEvent) => {
       if (!enableDrop.value || editMode.value !== EditMode.editable) return
       if (readonly.value || disable.value) return
@@ -284,28 +278,22 @@ export default defineComponent({
       processFile(e.dataTransfer.files[0])
     }
 
-    // 이미지 제거
     const removeImage = () => {
       if (editMode.value !== EditMode.editable) return
       const prev = imageValue.value
-
-      // 신규 업로드 파일인 경우 (fileId 없음, file 객체 있음)
       if (prev && prev.file && !prev.fileId) {
         imageValue.value = null
       }
-      // DB 파일인 경우 (fileId 있음)
       else if (prev && prev.fileId) {
-        // isDeleted 플래그만 추가
         imageValue.value = { ...prev, isDeleted: true }
       }
       setPreviewUrl(null)
       isModified.value = true
       emit('update:modelValue', imageValue.value)
-      emit('remove', prev, imageBoxMethods.value)
-      emit('change', imageValue.value, imageBoxMethods.value)
+      emit('remove', prev, imageboxMethods.value)
+      emit('change', imageValue.value, imageboxMethods.value)
     }
 
-    // 파일 선택
     const openFileDialog = () => { fileInputRef.value?.click() }
 
     const applyDataModel = <T extends AttachedFileItem>(dataModel: InterfaceDataModel<T>) => {
@@ -317,23 +305,23 @@ export default defineComponent({
     }
 
     const mount = () => {
-      if (hisonCloser.component.imageBoxList[id]) throw new Error(`[Hisonvue] imagebox id attribute was duplicated.`)
+      if (hisonCloser.component.imageboxList[id]) throw new Error(`[Hisonvue] imagebox id attribute was duplicated.`)
       registerReloadable(reloadId, () => {
         unmount()
         nextTick(mount)
       })
       if (!fileInputRef.value) return
-      refleshResponsiveClassList()
-      imageBoxMethods.value = {
+      refreshResponsiveClassList()
+      imageboxMethods.value = {
         getId: () => id,
-        getType: () => 'imageBox',
+        getType: () => 'imagebox',
         isVisible: () => visible.value,
         setVisible: (val: boolean) => visible.value = val,
         getEditMode: () => editMode.value,
         setEditMode: (val: EditMode) => { 
           editMode.value = val
-          hisonCloser.component.buttonList[`hison_input_imagebox_add_button_${id}`]?.setDisable(disable.value)
-          hisonCloser.component.buttonList[`hison_input_imagebox_remove_button_${id}`]?.setDisable(disable.value)
+          hisonCloser.component.buttonList[`hison_imagebox_add_button_${id}`]?.setDisable(disable.value)
+          hisonCloser.component.buttonList[`hison_imagebox_remove_button_${id}`]?.setDisable(disable.value)
         },
         getValue: () => hison.utils.deepCopyObject(imageValue.value),
         setValue: (val: AttachedFileItem) => {
@@ -369,31 +357,35 @@ export default defineComponent({
         setOnMaxFileSizeExceeded: (handler) => { onMaxFileSizeExceeded.value = handler },
         isModified: () => isModified.value,
         setModified: (val: boolean) => isModified.value = val,
-        focus: () => { hisonCloser.component.buttonList[`hison_input_imagebox_add_button_${id}`]?.focus() },
+        isBorder: () => border.value,
+        setBorder: (val: boolean) => { border.value = val },
+        getTabIndex: () => tabIndex.value,
+        setTabIndex: (v: number | null) => {
+          tabIndex.value = v !== null && v !== undefined ? Number(v) : null
+        },
+        focus: () => { hisonCloser.component.buttonList[`hison_imagebox_add_button_${id}`]?.focus() },
         reload: () => reloadHisonComponent(reloadId)
       }
-      hisonCloser.component.imageBoxList[id] = imageBoxMethods.value
-      emit('mounted', imageBoxMethods.value)
+      hisonCloser.component.imageboxList[id] = imageboxMethods.value
+      emit('mounted', imageboxMethods.value)
     }
     const unmount = () => {
-      if (hisonCloser.component.imageBoxList && hisonCloser.component.imageBoxList[id]) {
-        delete hisonCloser.component.imageBoxList[id]
+      if (hisonCloser.component.imageboxList && hisonCloser.component.imageboxList[id]) {
+        delete hisonCloser.component.imageboxList[id]
       }
     }
 
     onMounted(mount)
     onBeforeUnmount(unmount)
 
-    // prop/modelValue → 내부 반영
     watch(() => props.modelValue, (val) => {
       if (val !== imageValue.value) {
         imageValue.value = val
         setPreviewUrl(val)
       }
     })
-    // device prop 변경 대응
     watch(device, (newDevice) => {
-      refleshResponsiveClassList()
+      refreshResponsiveClassList()
       emit('responsive-change', newDevice)
     })
 
@@ -403,21 +395,23 @@ export default defineComponent({
       props,
       imgStyle: computed(() => props.imgStyle || {}),
       imageValue,
-      imageBoxMethods,
+      imageboxMethods,
       visibleClass,
       editModeClass,
+      borderClass,
       responsiveClassList,
       buttonClassList,
       disable,
       readonly,
-      addButtonTextHtml,
-      removeButtonTextHtml,
+      addButtonText,
+      removeButtonText,
       placeholder,
       isDragging,
       previewUrl,
       showAddButton,
       showRemoveButton,
       accept,
+      tabIndex,
 
       handleDragEnter,
       handleDrop,
@@ -429,5 +423,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
